e8dbb17a52c7071afb09bb306e858b7dc552b8ce
Okke Harsta <oharsta@zilverline.com>
2019-01-09 16:12:46 +0100

    Lookup Service by entity ID in the registration process

--- server/test/api/test_service.py
+++ server/test/api/test_service.py
@@ -4,12 +4,15 @@ from server.test.abstract_test import AbstractTest
 
 class TestService(AbstractTest):
 
+    def _find_mail_by_entity_id(self):
+        return self.get("/api/service/find_by_entity", query_data={"entity_id": "https://mail"})
+
     def test_find_by_entity_id(self):
-        mail = self.find_mail_by_entity_id()
+        mail = self._find_mail_by_entity_id()
         self.assertEqual("mail", mail["name"])
 
-    def find_mail_by_entity_id(self):
-        return self.get("/api/service/find_by_entity", query_data={"entity_id": "https://mail"})
+    def test_find_by_non_existing_entity_id(self):
+        self.get("/api/service/find_by_entity", query_data={"entity_id": "nope"}, response_status_code=500)
 
     def test_service_new(self):
         service = self.post("/api/service", body={"entity_id": "https://new_service", "name": "new_service"})
@@ -17,12 +20,12 @@ class TestService(AbstractTest):
         self.assertEqual("new_service", service["name"])
 
     def test_service_update(self):
-        mail = self.find_mail_by_entity_id()
+        mail = self._find_mail_by_entity_id()
         mail["name"] = "changed"
         service = self.put("/api/service", body=mail)
         self.assertEqual("changed", service["name"])
 
     def test_service_delete(self):
-        mail = self.find_mail_by_entity_id()
+        mail = self._find_mail_by_entity_id()
         self.delete("/api/service", primary_key=mail["id"])
         self.assertEqual(1, Service.query.count())
