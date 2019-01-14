6afa5214bdb9aebec68ed85c9c7bc88cbd96cfc3
Okke Harsta <oharsta@zilverline.com>
2019-01-14 08:40:49 +0100

    More attributes in the seed

--- server/test/api/test_user_service_profile.py
+++ server/test/api/test_user_service_profile.py
@@ -5,7 +5,4 @@ class TestUserServiceProfile(AbstractTest):
 
     def test_attributes(self):
         res = self.get("/api/user_service_profiles/attributes", query_data={"uid": "urn:john"})
-        self.assertListEqual(res, [
-            {"name": "John Doe", "service_entity_id": "https://network"},
-            {"email": "john@example.org", "global": True, "name": "John Doe", "uid": "urn:john"}
-        ])
+        self.assertEqual(res[0]["name"], "John Doe")
