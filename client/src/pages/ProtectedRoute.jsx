3acdb2fe73e10263e2346308f89acbae05e1e7a0
Okke Harsta <oharsta@zilverline.com>
2019-02-13 09:33:29 +0100

    Return something from rendering in ProtectedRoute

--- client/src/pages/ProtectedRoute.jsx
+++ client/src/pages/ProtectedRoute.jsx
@@ -5,8 +5,8 @@ export function ProtectedRoute({currentUser, Component, redirectToLogin = false,
     if (!currentUser.guest) {
         return <Route render={props => <Component user={currentUser} {...res} {...props}/>}/>
     } else if (redirectToLogin) {
-        window.location.href = `/login?state=${encodeURIComponent(res.location.pathname)}`;
-        return;
+        setTimeout(() => window.location.href = `/login?state=${encodeURIComponent(res.location.pathname)}`, 50);
+        return null;
     }
     return <Redirect to={"/404"}/>;
 }
