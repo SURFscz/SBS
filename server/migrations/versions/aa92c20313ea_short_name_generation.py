6bce843d9077664e43cbea9c6552dfbdb66115f6
Okke Harsta <oharsta@zilverline.com>
2019-09-12 14:00:28 +0200

    Provision username for users if nonee

--- server/migrations/versions/aa92c20313ea_short_name_generation.py
+++ server/migrations/versions/aa92c20313ea_short_name_generation.py
@@ -25,17 +25,6 @@ def upgrade():
     conn.execute(text("ALTER TABLE users ADD COLUMN username varchar(255)"))
     conn.execute(text("ALTER TABLE users ADD UNIQUE INDEX users_username(username)"))
 
-    # from server.api.user import generate_unique_username
-    # from server.db.db import User, db
-    # from server.__main__ import app
-    #
-    # with app.app_context():
-    #     users = User.query.filter(User.username == None).all()  # noqa: E711
-    #     for user in users:
-    #         user.username = generate_unique_username(user)
-    #         db.session.merge(user)
-    #         db.session.commit()
-
 
 def downgrade():
     pass
