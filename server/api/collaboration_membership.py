85bf21f22c50e8d81d0f209c1601f2135732fa08
Okke Harsta <oharsta@zilverline.com>
2019-11-01 15:34:20 +0100

    Clean up user_service_profile's when membership is deleted

--- server/api/collaboration_membership.py
+++ server/api/collaboration_membership.py
@@ -1,9 +1,9 @@
 # -*- coding: future_fstrings -*-
 from flask import Blueprint, request as current_request
-
+from server.api.base import ctx_logger
 from server.api.base import json_endpoint
 from server.auth.security import confirm_collaboration_admin
-from server.db.db import CollaborationMembership, db
+from server.db.db import CollaborationMembership, db, UserServiceProfile, AuthorisationGroup, Collaboration
 
 collaboration_membership_api = Blueprint("collaboration_membership_api", __name__,
                                          url_prefix="/api/collaboration_memberships")
@@ -14,10 +14,24 @@ collaboration_membership_api = Blueprint("collaboration_membership_api", __name_
 def delete_collaboration_membership(collaboration_id, user_id):
     confirm_collaboration_admin(collaboration_id)
 
+    user_service_profiles = UserServiceProfile.query \
+        .join(UserServiceProfile.authorisation_group) \
+        .join(AuthorisationGroup.collaboration) \
+        .filter(Collaboration.id == collaboration_id) \
+        .filter(UserServiceProfile.user_id == user_id) \
+        .all()
+    for usp in user_service_profiles:
+        db.session.delete(usp)
+
+    logger = ctx_logger("collaboration_membership_api")
+    logger.info(f"Deleted {len(user_service_profiles)} user_service_profiles of {user_id}")
+
     row_count = CollaborationMembership.query \
         .filter(CollaborationMembership.collaboration_id == collaboration_id) \
         .filter(CollaborationMembership.user_id == user_id) \
         .delete()
+
+    logger.info(f"Deleted {row_count} collaboration memberships of {user_id}")
     return (None, 204) if row_count > 0 else (None, 404)
 
 
