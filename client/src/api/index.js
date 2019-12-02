578afa58fe72f4daa8d88f015a849a0af6cc3e03
Okke Harsta <oharsta@zilverline.com>
2019-12-02 13:05:03 +0100

    Upgraded JS dependencies

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -94,9 +94,9 @@ export function config() {
 
 //Users
 export function me(config) {
-    const headers = (config.local) ? {
-        "OIDC_CLAIM_cmuid": "urn:john",
-        // "OIDC_CLAIM_cmuid": "urn:temp",
+    const headers = (config.local && false) ? {
+        // "OIDC_CLAIM_cmuid": "urn:john",
+        "OIDC_CLAIM_cmuid": "urn:temp",
         "OIDC_CLAIM_Nickname": "jëhny",
         "OIDC_CLAIM_Edumember-Is-Member-Of": "Release 0.6:CO:members:all,Release 0.6:CO:members:active",
         "OIDC_CLAIM_Eduperson-Affiliation": "librarywalkin",
