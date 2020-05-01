de47d191f8068ca8cf135d25d801705a544c07f2
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2020-05-01 08:07:07 +0200

    Make exported SAML attributes compliant with AARC-G002 and fix tests

--- server/test/api/test_user_saml.py
+++ server/test/api/test_user_saml.py
@@ -9,21 +9,16 @@ class TestUserSaml(AbstractTest):
         res = self.get("/api/users/attributes",
                        query_data={"uid": "urn:john", "service_entity_id": "https://network"})
 
-        self.assertSetEqual(set(res.keys()), {
-            "urn:mace:dir:attribute-def:cn", "urn:mace:dir:attribute-def:eduPersonPrincipalName",
-            "urn:mace:dir:attribute-def:isMemberOf", "urn:mace:dir:attribute-def:mail",
-            "urn:mace:dir:attribute-def:postalAddress", "urn:mace:dir:attribute-def:shortName",
-            "urn:mace:dir:attribute-def:uid", "urn:oid:1.3.6.1.4.1.24552.1.1.1.13"})
+        self.assertSetEqual(set(res.keys()), {"cuid", "uid", "sshKey", "eduPersonEntitlement"})
 
-        self.assertListEqual(res["urn:mace:dir:attribute-def:postalAddress"], ["Postal 1234AA"])
-        self.assertListEqual(res["urn:mace:dir:attribute-def:cn"], ["John Doe"])
-        self.assertListEqual(sorted(res["urn:mace:dir:attribute-def:isMemberOf"]), ["ai_computing", "ai_dev", "ai_res"])
-        self.assertListEqual(sorted(res["urn:mace:dir:attribute-def:mail"]), ["john@example.org"])
-        self.assertListEqual(res["urn:mace:dir:attribute-def:uid"], ["urn:john"])
-        self.assertListEqual(res["urn:mace:dir:attribute-def:shortName"], ["john"])
-        self.assertEqual(1, len(res["urn:oid:1.3.6.1.4.1.24552.1.1.1.13"]))
-        self.assertListEqual(res["urn:mace:dir:attribute-def:eduPersonPrincipalName"],
-                             ["john@ai_computing.test.sbs.local"])
+        self.assertListEqual(res["cuid"], ["urn:john"])
+        self.assertListEqual(res["uid"], ["john"])
+        self.assertEqual(1, len(res["sshKey"]))
+        self.assertSetEqual(set(res["eduPersonEntitlement"]), {
+            "urn:example:sbs:group:ai_computing",
+            "urn:example:sbs:group:ai_computing:ai_dev",
+            "urn:example:sbs:group:ai_computing:ai_res"
+        })
 
     def test_attributes_no_service(self):
         res = self.get("/api/users/attributes",
