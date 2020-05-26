30275cd1ab17ac85ffbb259f11ed2967a0915e58
Martin van Es <martin@mrvanes.com>
2020-05-26 12:58:09 +0000

    Remove obsolete var and keep connection alive

--- server/mqtt.py
+++ server/mqtt.py
@@ -4,7 +4,6 @@ import paho.mqtt.client as client
 
 class mqttClient(client.Client):
     enabled = False
-    client = None
 
     def __init__(self, app, *args, **kwargs):
         self.enabled = app.app_config.service_bus.enabled
@@ -15,7 +14,7 @@ class mqttClient(client.Client):
         super(mqttClient, self).__init__(client_id, clean_session=False, *args, **kwargs)
         self.username_pw_set(user, password=password)
         if self.enabled:
-            self.connect(host)
+            self.connect(host, keepalive=60)
 
     def publish(self, *args, **kwargs):
         if self.enabled:
