22ff1df60a2c74db31b1d38a8248da604799a289
Okke Harsta <oharsta@zilverline.com>
2020-05-27 07:35:29 +0200

    BSR

--- server/test/mqtt/test_mqtt.py
+++ server/test/mqtt/test_mqtt.py
@@ -7,30 +7,28 @@ from server.mqtt.mqtt import MqttClient
 from server.test.abstract_test import AbstractTest
 
 
-@asyncio.coroutine
-def start_broker():
-    config = {
-        "listeners": {
-            "default": {
-                "max-connections": 1,
-                "type": "tcp",
-                "bind": "localhost:1883"
-            },
-        },
-        "auth": {
-            "plugins": ["auth.anonymous"],
-            "allow-anonymous": True}
-    }
-    broker = Broker(config)
-
-    yield from broker.start()
-
-
 class TestMqtt(AbstractTest):
 
+    @asyncio.coroutine
+    def start_broker(self):
+        config = {
+            "listeners": {
+                "default": {
+                    "max-connections": 1,
+                    "type": "tcp",
+                    "bind": "localhost:1883"
+                },
+            },
+            "auth": {
+                "plugins": ["auth.anonymous"],
+                "allow-anonymous": True}
+        }
+        self.broker = Broker(config)
+
+        yield from self.broker.start()
+
     def test_publish(self):
-        asyncio.get_event_loop().run_until_complete(start_broker())
-        # self.start_broker()
+        asyncio.get_event_loop().run_until_complete(self.start_broker())
         config_service_bus = munchify({
             "enabled": True,
             "user": "anonymous",
@@ -40,4 +38,4 @@ class TestMqtt(AbstractTest):
         })
         mqtt_client = MqttClient(config_service_bus)
         mqtt_client.publish("test", "1", qos=1, retain=False)
-        # self.broker.shutdown()
+        self.broker.shutdown()
