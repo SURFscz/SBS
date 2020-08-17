f65e8d976efc86f4a14bcb9101cfd9cba8fafbdc
Okke Harsta <oharsta@zilverline.com>
2020-08-17 10:34:09 +0200

    Allow Ed25519 public keys

--- client/src/validations/regExps.js
+++ client/src/validations/regExps.js
@@ -1,6 +1,7 @@
 export const validEmailRegExp = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 export const validNameRegExp = /^[\w \-']{1,255}$/;
 export const validPublicSSHKeyRegExp = /ssh-rsa AAAA[0-9A-Za-z+/]+[=]{0,3}( [^@]+@[^@]+)?/;
+export const validPublicSSHEd25519KeyRegExp = /ssh-ed25519 AAAA[0-9A-Za-z+/]+[=]{0,3}( [^@]+@[^@]+)?/;
 export const validPublicSSH2KeyRegExp = /---- BEGIN SSH2 PUBLIC KEY ----.*/;
 export const sanitizeShortName = shortName => shortName ? shortName.replace(/[^a-zA-Z_0-9]+/g, "").substring(0, 32).toLowerCase() : shortName;
 export const shortNameDisabled = (user, isNew, adminOfEntity ) => !(user.admin || (isNew && adminOfEntity));
