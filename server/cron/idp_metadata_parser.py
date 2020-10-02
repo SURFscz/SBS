4424671062f580d691fffe80fa60bb7b68405795
Okke Harsta <oharsta@zilverline.com>
2020-10-02 15:06:36 +0200

    We parse scopes, not entities ID's

--- server/cron/idp_metadata_parser.py
+++ server/cron/idp_metadata_parser.py
@@ -12,29 +12,33 @@ def parse_idp_metadata(app):
     metadata = app.app_config.metadata
     pre = request.urlopen(metadata.idp_url)
     results = {}
-    display_name_nl = display_name_en = entity_id = None
+    display_name_nl = display_name_en = None
+    scopes = []
     for event, element in ET.iterparse(pre, events=("start", "end")):
         if "}" in element.tag:
-            element.tag = element.tag.split('}', 1)[1]
-        if event == "start" and element.tag == "EntityDescriptor":
-            entity_id = element.get("entityID")
-        if event == "start" and element.tag == "DisplayName":
+            element.tag = element.tag.split("}", 1)[1]
+        if event == "start" and element.tag == "Scope":
+            scopes.append(element.text)
+
+        elif event == "start" and element.tag == "DisplayName":
             lang = element.attrib.get('{http://www.w3.org/XML/1998/namespace}lang')
             if lang == "nl":
                 display_name_nl = element.text
             elif lang == "en":
                 display_name_en = element.text
 
-        if event == "end" and element.tag == "EntityDescriptor" and (display_name_nl or display_name_en):
-            results[entity_id] = {"nl": display_name_nl if display_name_nl else display_name_en,
-                                  "en": display_name_en if display_name_en else display_name_nl}
-            display_name_nl = display_name_en = entity_id = None
+        elif event == "end" and element.tag == "EntityDescriptor" and (display_name_nl or display_name_en):
+            for scope in scopes:
+                results[scope] = {"nl": display_name_nl or display_name_en,
+                                  "en": display_name_en or display_name_nl}
+            display_name_nl = display_name_en = None
+            scopes = []
 
     idp_metadata = results
 
 
-def idp_display_name(entity_id, lang="en"):
+def idp_display_name(schac_home_organization, lang="en"):
     if not idp_metadata:
         parse_idp_metadata(current_app)
-    names = idp_metadata.get(entity_id, {"en": entity_id})
+    names = idp_metadata.get(schac_home_organization, {"en": schac_home_organization})
     return names[lang] if lang in names else names["en"]
