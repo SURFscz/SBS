2120febd1c4040a8705a464f7ba996b020673ac2
Okke Harsta <oharsta@zilverline.com>
2020-10-12 13:31:29 +0200

    schachome from original eppn of user

--- server/test/auth/test_user_claims.py
+++ server/test/auth/test_user_claims.py
@@ -18,7 +18,12 @@ class TestUserClaims(AbstractTest):
 
     def test_add_user_claims_affiliation(self):
         user = User()
-        add_user_claims({"eduperson_scoped_affiliation": ["teacher@sub.uni.org"]}, "urn:johny", user)
+        add_user_claims({"voperson_external_id": "teacher@university"}, "urn:johny", user)
+        self.assertEqual("university", user.schac_home_organisation)
+
+    def test_add_user_claims_affiliation(self):
+        user = User()
+        add_user_claims({"voperson_external_id": ["teacher@sub.uni.org"]}, "urn:johny", user)
         self.assertEqual("uni.org", user.schac_home_organisation)
 
     def test_add_user_claims_user_name(self):
@@ -45,3 +50,8 @@ class TestUserClaims(AbstractTest):
     def test_generate_unique_username_random(self):
         username = generate_unique_username(munchify({"given_name": "John", "family_name": "Doe"}), 1)
         self.assertEqual(14, len(username))
+
+    def test_eppn_generate_unique_username(self):
+        user = User(eduperson_principal_name="sarah-lee")
+        username = generate_unique_username(user)
+        self.assertEqual("sarahlee", username)
