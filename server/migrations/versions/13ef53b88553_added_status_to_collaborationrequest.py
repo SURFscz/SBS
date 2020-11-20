6e69b13b92542922549db9c4cd01b324aae08bcb
Okke Harsta <oharsta@zilverline.com>
2020-11-20 16:23:20 +0100

    Flake8

--- server/migrations/versions/13ef53b88553_added_status_to_collaborationrequest.py
+++ server/migrations/versions/13ef53b88553_added_status_to_collaborationrequest.py
@@ -8,7 +8,6 @@ Create Date: 2020-11-20 15:28:39.447128
 from alembic import op
 from sqlalchemy import text
 
-
 # revision identifiers, used by Alembic.
 revision = '13ef53b88553'
 down_revision = 'f6d7819e5a39'
@@ -21,6 +20,7 @@ def upgrade():
     conn.execute(text("ALTER TABLE collaboration_requests ADD COLUMN status varchar(255)"))
     conn.execute("UPDATE collaboration_requests SET status = 'open'")
     conn.execute(text("ALTER TABLE collaboration_requests CHANGE status status VARCHAR(255) NOT NULL"))
+
 
 def downgrade():
     pass
