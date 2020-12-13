2cee9fb7131dfdc92df2a622c4a9c286bb9da2ba
Okke Harsta <oharsta@zilverline.com>
2020-12-13 11:43:00 +0100

    Landingpage header

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -98,7 +98,7 @@ export function authorizationUrl(state) {
 }
 
 export function me(config) {
-    if (config.local && true) {
+    if (config.local && false) {
         let sub = "urn:john";
         // sub = "urn:james";
         // sub = "urn:betty";
