3c8eb118e1cb15983ea6a64ed41d8c4cd18608c5
Okke Harsta <oharsta@zilverline.com>
2020-12-16 19:09:53 +0100

    404

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,23 +1,18 @@
 import React from "react";
-import I18n from "i18n-js";
 import "./NotFound.scss";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {Planet} from 'react-kawaii'
-import {login} from "../utils/Login";
+import {Player} from '@lottiefiles/react-lottie-player';
+import notFound from "../lotties/not_found.json"
+
 
 export default function NotFound({currentUser}) {
     return (
         <div className="mod-not-found">
-            <div className="title">
-                <p dangerouslySetInnerHTML={{__html: I18n.t("not_found.description_html")}}/>
-                {currentUser.guest && <a href="/login" onClick={login}>
-                    <FontAwesomeIcon icon="arrow-right"/>{I18n.t("not_found.loginLink")}
-                </a>}
-            </div>
-            <div className="content">
-                <Planet size={80} mood="sad" color="white"/>
-                <p className="hero">{I18n.t("not_found.title")}</p>
-            </div>
+            <Player
+                autoplay
+                loop
+                src={notFound}
+                style={{height: '300px', width: '300px'}}
+            />
         </div>
     );
 
