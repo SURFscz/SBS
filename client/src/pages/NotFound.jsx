d039739c3d349b9f8a5a1be3d689dcb06f545d74
Okke Harsta <oharsta@zilverline.com>
2020-12-28 15:20:00 +0100

    eslint

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ import {Player} from '@lottiefiles/react-lottie-player';
 import notFound from "../lotties/not_found.json"
 
 
-export default function NotFound({}) {
+export default function NotFound() {
     return (
         <div className="mod-not-found">
             <Player
