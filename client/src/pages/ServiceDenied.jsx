f7c0013ca42b04505d3aaac97c85d64ea14a5449
Okke Harsta <oharsta@zilverline.com>
2021-06-24 10:04:25 +0200

    Escape service name in order to show html

--- client/src/pages/ServiceDenied.jsx
+++ client/src/pages/ServiceDenied.jsx
@@ -3,6 +3,7 @@ import "./ServiceDenied.scss";
 import {Player} from "@lottiefiles/react-lottie-player";
 import service_denied from "../lotties/service-denied.json";
 import I18n from "i18n-js";
+import escape from "lodash.escape";
 
 export default function ServiceDenied() {
 
@@ -13,20 +14,20 @@ export default function ServiceDenied() {
         const urlSearchParams = new URLSearchParams(window.location.search);
         const name = urlSearchParams.get("service_name");
         const status = urlSearchParams.get("error_status");
-        setServiceName(name);
+        setServiceName(escape(name));
         setStatus(status);
     }, []);
 
     return (
         <div className="mod-service-denied">
             <div className="content">
-                <h1>{I18n.t("sfo.title", {name: serviceName})}</h1>
+                <h1 dangerouslySetInnerHTML={{__html: I18n.t("sfo.title", {name: serviceName})}}/>
                 <Player
                     autoplay
                     loop
                     src={service_denied}
                     style={{height: "auto", width: "85px", "maxWidth": "85px"}}/>
-                <span>{I18n.t(`sfo.info${status}`, {name: serviceName})}</span>
+                {<span dangerouslySetInnerHTML={{__html: I18n.t(`sfo.info${status}`, {name: serviceName})}}/>}
             </div>
         </div>
     );
