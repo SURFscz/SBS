cd13e693607de37ce66f5793b2064d7245d01dcf
Okke Harsta <oharsta@zilverline.com>
2021-07-01 12:01:12 +0200

    Flake8

--- server/test/cron/test_collaboration_expiration.py
+++ server/test/cron/test_collaboration_expiration.py
@@ -12,28 +12,27 @@ from server.test.seed import ai_computing_name, uva_research_name, uuc_teachers_
 
 class TestCollaborationExpiration(AbstractTest):
 
-    def test_schedule(self):
+    def _setup_data(self):
         now = datetime.datetime.utcnow()
         cfq = self.app.app_config.collaboration_expiration
-
         # Will cause expiration warning mail
         threshold_upper = datetime.timedelta(days=cfq.expired_warning_mail_days_threshold)
         coll = self.find_entity_by_name(Collaboration, ai_computing_name)
         coll.expiry_date = now + threshold_upper - datetime.timedelta(hours=12)
         db.session.merge(coll)
-
         coll = self.find_entity_by_name(Collaboration, uva_research_name)
         # Will cause expiration and notification mail
         coll.expiry_date = now - datetime.timedelta(days=3)
         db.session.merge(coll)
-
         coll = self.find_entity_by_name(Collaboration, uuc_teachers_name)
         # Will cause deletion
         coll.expiry_date = now - datetime.timedelta(days=365 * 5)
         coll.status = STATUS_EXPIRED
         db.session.merge(coll)
-
         db.session.commit()
+
+    def test_schedule(self):
+        self._setup_data()
 
         mail = self.app.mail
         with mail.record_messages() as outbox:
@@ -46,5 +45,12 @@ class TestCollaborationExpiration(AbstractTest):
             self.assertEqual(uuc_teachers_name, results["collaborations_deleted"][0]["name"])
             self.assertEqual(2, len(outbox))
 
+        self.assertEqual(0, Collaboration.query.filter(Collaboration.name == uuc_teachers_name).count())
+        self.assertEqual(STATUS_EXPIRED, self.find_entity_by_name(Collaboration, uva_research_name).status)
+
+    def test_system_expire_collaborations(self):
+        self._setup_data()
+
+        self.put("/api/system/expire_collaborations")
         self.assertEqual(0, Collaboration.query.filter(Collaboration.name == uuc_teachers_name).count())
         self.assertEqual(STATUS_EXPIRED, self.find_entity_by_name(Collaboration, uva_research_name).status)
