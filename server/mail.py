d037dbb2d75edef46e56da9defbee4291b56307e
Okke Harsta <oharsta@zilverline.com>
2021-07-13 15:18:38 +0200

    Do not spam org members when coll user is expired

--- server/mail.py
+++ server/mail.py
@@ -86,12 +86,12 @@ def _store_mail(user, mail_type, recipients):
     db.session.commit()
 
 
-def _get_coll_emails(collaboration, mail_type):
-    org_members = [m.user for m in collaboration.organisation.organisation_memberships]
-    coll_admin = [m.user for m in collaboration.collaboration_memberships if m.role == "admin"]
-    recipients = org_members + coll_admin
-    emails = [r.email for r in recipients]
-    for r in recipients:
+def _get_coll_emails(collaboration, mail_type, include_org_members=True):
+    coll_admins = [m.user for m in collaboration.collaboration_memberships if m.role == "admin"]
+    if include_org_members:
+        coll_admins += [m.user for m in collaboration.organisation.organisation_memberships]
+    emails = [r.email for r in coll_admins]
+    for r in coll_admins:
         _store_mail(r, mail_type, emails)
     return emails
 
@@ -357,7 +357,7 @@ def mail_collaboration_suspension_notification(collaboration, is_warning):
 
 def mail_membership_expires_notification(membership, is_warning):
     mail_type = MEMBERSHIP_EXPIRES_WARNING_MAIL if is_warning else MEMBERSHIP_EXPIRED_NOTIFICATION_MAIL
-    recipients = _get_coll_emails(membership.collaboration, mail_type)
+    recipients = _get_coll_emails(membership.collaboration, mail_type, include_org_members=False)
 
     threshold = current_app.app_config.membership_expiration.expired_warning_mail_days_threshold
     if is_warning:
