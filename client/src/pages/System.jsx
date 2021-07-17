33eac864a746fe7232f88ddd16f9d2cbaf9026b3
Okke Harsta <oharsta@zilverline.com>
2021-07-17 12:17:03 +0200

    Show count

--- client/src/pages/System.jsx
+++ client/src/pages/System.jsx
@@ -172,7 +172,7 @@ class System extends React.Component {
                 <div className="mod-system">
                     <section className={"info-block-container"}>
                         <section className="search-activity">
-                            <p>{I18n.t("system.activity", {count: filteredAuditLogs.length})}</p>
+                            <p>{I18n.t("system.activity", {count: filteredAuditLogs.audit_logs.length})}</p>
                             {config.seed_allowed &&
                             <Button warningButton={true} onClick={() => this.doClearAuditLogs(true)}/>}
                             <div className={`search ${config.seed_allowed ? "" : "no-clear-logs"}`}>
