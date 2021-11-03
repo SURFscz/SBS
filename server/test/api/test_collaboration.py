85ae9d20a3cbc58fa951e01d3e082e94a34cfc17
Okke Harsta <oharsta@zilverline.com>
2021-11-03 08:55:46 +0100

    Delete print

--- server/test/api/test_collaboration.py
+++ server/test/api/test_collaboration.py
@@ -81,7 +81,6 @@ class TestCollaboration(AbstractTest):
             self.assertEqual("uuc:new_short_name", collaboration["global_urn"])
             self.assertEqual(STATUS_ACTIVE, collaboration["status"])
             self.assertEqual(2, len(outbox))
-            print(outbox[0].html)
             self.assertTrue(
                 "You have been invited by urn:john to join collaboration 'new_collaboration'" in outbox[0].html)
 
