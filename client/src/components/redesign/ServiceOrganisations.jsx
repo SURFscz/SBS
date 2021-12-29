a54d5b3377071e907cafe2dab7b0246c06630c60
Okke Harsta <oharsta@zilverline.com>
2021-12-29 12:01:32 +0100

    Open the correct link in new tab for entity overview

--- client/src/components/redesign/ServiceOrganisations.jsx
+++ client/src/components/redesign/ServiceOrganisations.jsx
@@ -152,7 +152,8 @@ class ServiceOrganisations extends React.Component {
             {
                 key: "name",
                 header: I18n.t("models.organisations.name"),
-                mapper: org => userServiceAdmin ? <span>{org.name}</span> : <a href="/" onClick={this.openOrganisation(org)}>{org.name}</a>,
+                mapper: org => userServiceAdmin ? <span>{org.name}</span> :
+                    <a href={`/organisations/${org.id}`} onClick={this.openOrganisation(org)}>{org.name}</a>,
             },
             {
                 key: "short_name",
