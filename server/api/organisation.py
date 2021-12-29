1e800ecdcca8b18a769a9a643704fd8e6d94a9b0
Okke Harsta <oharsta@zilverline.com>
2021-12-29 11:32:04 +0100

    Organisation API endpoint

--- server/api/organisation.py
+++ server/api/organisation.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import selectinload
 
 from server.api.base import json_endpoint, query_param, replace_full_text_search_boolean_mode_chars
 from server.auth.security import confirm_write_access, current_user_id, is_application_admin, \
-    confirm_organisation_admin, generate_token, is_service_admin
+    confirm_organisation_admin, generate_token, is_service_admin, confirm_external_api_call
 from server.cron.idp_metadata_parser import idp_display_name
 from server.db.db import db
 from server.db.defaults import default_expiry_date, cleanse_short_name
@@ -106,6 +106,16 @@ def my_organisations_lite():
             .filter(OrganisationMembership.role.in_(["admin", "manager"]))
     organisations = query.all()
     return organisations, 200
+
+
+@organisation_api.route("/v1", strict_slashes=False)
+@json_endpoint
+def api_organisation_details():
+    confirm_external_api_call()
+    organisation = request_context.external_api_organisation
+    organisation.collaborations
+
+    return organisation, 200
 
 
 @organisation_api.route("/<organisation_id>", strict_slashes=False)
