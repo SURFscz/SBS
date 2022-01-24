979c6a7d917bd9994804c8e73392f182e035fff9
Okke Harsta <oharsta@zilverline.com>
2022-01-24 10:02:05 +0100

    Swagger bulk invite

--- server/api/invitation.py
+++ server/api/invitation.py
@@ -49,6 +49,11 @@ def do_resend(invitation_id):
     }, invitation.collaboration, [invitation.invitee_email])
 
 
+def parse_date(val, default_date=None):
+    return datetime.datetime.fromtimestamp(val / 1e3) if val and (
+            isinstance(val, float) or isinstance(val, int)) else default_date
+
+
 @invitations_api.route("/find_by_hash", strict_slashes=False)
 @json_endpoint
 def invitations_by_hash():
@@ -98,8 +103,8 @@ def collaboration_invites_api():
 
     message = data.get("message")
     intended_role = data.get("intended_role", "member")
-    expiry_date = data.get("invitation_expiry_date", default_expiry_date())
-    membership_expiry_date = data.get("membership_expiry_date", None)
+    expiry_date = parse_date(data.get("invitation_expiry_date"), default_expiry_date())
+    membership_expiry_date = parse_date(data.get("membership_expiry_date"))
     invites = list(filter(lambda recipient: bool(email_re.match(recipient)), data["invites"]))
     invites_results = []
     for email in invites:
