00e42410f2239bbd872f4d37dd7155f3e2aa26a5
Okke Harsta <oharsta@zilverline.com>
2022-01-24 10:56:51 +0100

    Do not return 304 for swagger schema files

--- server/swagger/conf.py
+++ server/swagger/conf.py
@@ -35,4 +35,8 @@ swagger_specs = Blueprint("swagger_specs", __name__, url_prefix="/swagger")
 
 @swagger_specs.get("/<path:filename>")
 def base_static(filename):
-    return send_from_directory(current_app.root_path + "/swagger", filename)
+    result = send_from_directory(current_app.root_path + "/swagger", filename)
+    result.status_code = 200
+    result.cache_control.clear()
+    result.cache_control.max_age = 60 * 60
+    return result
--- server/test/swagger/test_swagger.py
+++ server/test/swagger/test_swagger.py
@@ -8,5 +8,8 @@ class TestSwagger(AbstractTest):
 
     def test_path(self):
         res = self.client.get("/swagger/schemas/CollaborationDetail.yaml")
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual("max-age=3600", res.headers.get("Cache-Control"))
+
         parsed = yaml.load(res.data, Loader=yaml.FullLoader)
         self.assertEqual("Researcher", parsed["properties"]["name"]["example"])
