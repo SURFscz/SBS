a623de663799b7db85de0545a5dfdd84f390980c
Okke Harsta <oharsta@zilverline.com>
2022-04-26 16:36:31 +0200

    Use config for SAML directory

--- server/auth/ssid.py
+++ server/auth/ssid.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import request as current_request, redirect, session
+from flask import request as current_request, redirect, session, current_app
 from onelogin.saml2.auth import OneLogin_Saml2_Auth
 
 AUTHN_REQUEST_ID = "AuthNRequestID"
@@ -8,7 +8,7 @@ USER_UID = "UserUID"
 
 
 def saml_auth():
-    saml_dir = "saml_test" if "TESTING" in os.environ and os.environ["TESTING"] == "1" else "saml"
+    saml_dir = current_app.app_config.ssid_config_folder
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "config", saml_dir)
     req = {
         "https": "on" if current_request.scheme == "https" else "off",
