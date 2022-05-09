696abd1ac58908967d4a52478bb3af3b260aee90
Floris Fokkinga <floris.fokkinga@surf.nl>
2022-05-09 15:55:26 +0200

    Updated translation: explanation sliders

--- client/src/components/explanations/ServicesGroups.jsx
+++ client/src/components/explanations/ServicesGroups.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import I18n from "i18n-js";
 
 export default function ServiceGroupsExplanation() {
-    return [
-        <section key={1} className="explanation">
-            You can add <span className="strong">Service Groups </span>to a <span
-            className="strong">Service </span>and for each of these <span className="strong">Service Groups</span> a <span
-            className="strong">Group </span> will be created in every <span className="strong">Collaboration</span> where this <span
-            className="strong">Service </span> is connected to.
-        </section>,
-        <section key={2} className="details">
-            If the <span className="strong">Service Group</span> is configured to auto provision members
-            then all <span className="strong">Members</span> of the <span className="strong">Collaboration</span> are
-            automatically added to the created <span className="strong">Group</span>.
-        </section>
-    ]
+    if (I18n.locale === "en") {
+        return [
+            <section key={1} className="explanation">
+                Service groups are predefined groups which are automatically created in a collaboration whenever the service is connected to the collaboration.
+                For example, if a service requires users to be in the group 'Create files' to be authorized to create new files in the service, a service admin can provision collaborations that allow access to that service with a group with that name. The collaboration admin can then provision the group with members.
+            </section>,
+            <section key={2} className="details">
+                Refer to the <a href="https://edu.nl/e9x8x">documentation</a> for details.
+            </section>
+        ];
+    }
+        return [
+            <section key={1} className="explanation">
+                Dienstgroepen zijn voorgedefinieerde groepen die automatisch worden toegevoegd aan een samenwerking op het moment van koppelen met en dienst.
+                Bijvoorbeeld, een dienst vereist dat gebruikers lid zijn van de groep 'bestanden maken' om geautoriseerd te zijn bestanden te maken in die dienst. De dienstbeheerder kan samenwerkingen die toegang tot de dienst toestaan voorzien van die groep. De samenwerkingsbeheerder kan de groepen dan vullen met leden.
+            </section>,
+            <section key={2} className="details">
+                Zie de <a href="https://edu.nl/e9x8x">documentatie</a> voor details.
+            </section>
+        ]
 
 }
