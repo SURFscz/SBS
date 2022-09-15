39c2586df5404693b160f8970ca6cdd244fc4751
Okke Harsta <oharsta@zilverline.com>
2022-09-15 09:11:21 +0200

    Fixes #333

--- client/src/validations/regExps.js
+++ client/src/validations/regExps.js
@@ -7,8 +7,18 @@ export const validSchacHomeRegExp = /^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9]
 export const validUrlRegExp = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
 
 const validPrefixes = [
-    "ssh-rsa", "ssh-ed25519", "ecdsa-sha2-nistp256", "ecdsa-sha2-nistp384", "ecdsa-sha2-nistp521",
-    "---- BEGIN SSH2 PUBLIC KEY ----", "-----BEGIN PUBLIC KEY-----", "-----BEGIN RSA PUBLIC KEY-----"
+  "---- BEGIN SSH2 PUBLIC KEY ----",
+  "-----BEGIN PUBLIC KEY-----",
+  "-----BEGIN RSA PUBLIC KEY-----",
+  "ecdsa-sha2-nistp256",
+  "ecdsa-sha2-nistp384",
+  "ecdsa-sha2-nistp521",
+  "sk-ecdsa-sha2-nistp256@openssh.com",
+  "sk-ssh-ed25519@openssh.com",
+  "ssh-dss",
+  "ssh-ed25519",
+  "ssh-ed25519",
+  "ssh-rsa"
 ]
 
 export const validateSSHKey = sshKey => {
