99955901d31e573a5971e7e5a6243b673005c1f9
Okke Harsta <oharsta@zilverline.com>
2022-11-02 14:02:23 +0100

    Fixes #366

--- server/db/domain.py
+++ server/db/domain.py
@@ -741,7 +741,7 @@ class PamSSOSession(Base, db.Model):
     session_id = db.Column("session_id", db.String(length=255), nullable=True)
     attribute = db.Column("attribute", db.String(length=255), nullable=True)
     pin = db.Column("pin", db.String(length=255), nullable=True)
-    user_id = db.Column(db.Integer(), db.ForeignKey("users.id"))
+    user_id = db.Column(db.Integer(), db.ForeignKey("users.id"), nullable=True)
     user = db.relationship("User")
     service_id = db.Column(db.Integer(), db.ForeignKey("services.id"))
     service = db.relationship("Service")
