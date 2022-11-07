727d5af7b5d855083f5ee404c183605df29486d1
Okke Harsta <oharsta@zilverline.com>
2022-11-07 16:27:01 +0100

    Test the non-happy path

--- server/test/scim/test_scim.py
+++ server/test/scim/test_scim.py
@@ -3,10 +3,10 @@ import json
 
 import responses
 
-from server.db.domain import User, Collaboration
+from server.db.domain import User, Collaboration, Group
 from server.scim.scim import apply_user_change, apply_group_change
 from server.test.abstract_test import AbstractTest
-from server.test.seed import sarah_name, uva_research_name
+from server.test.seed import sarah_name, uva_research_name, ai_researchers_group
 from server.tools import read_file
 
 
@@ -19,6 +19,15 @@ class TestScim(AbstractTest):
         user_created = json.loads(read_file("test/scim/user_created.json"))
         with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
             rsps.add(responses.GET, "http://localhost:9002/Users", json=no_user_found, status=200)
+            rsps.add(responses.POST, "http://localhost:9002/Users", json=user_created, status=201)
+            apply_user_change(sarah)
+
+    @responses.activate
+    def test_apply_user_change_create_with_invalid_response(self):
+        sarah = self.find_entity_by_name(User, sarah_name)
+        user_created = json.loads(read_file("test/scim/user_created.json"))
+        with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
+            rsps.add(responses.GET, "http://localhost:9002/Users", json={}, status=400)
             rsps.add(responses.POST, "http://localhost:9002/Users", json=user_created, status=201)
             apply_user_change(sarah)
 
@@ -86,3 +95,25 @@ class TestScim(AbstractTest):
             rsps.add(responses.DELETE, "http://localhost:9002/Groups/8d85ea05-fc5c-4222-8efd-130ff7938ee1",
                      status=201)
             apply_group_change(collaboration, deletion=True)
+
+    @responses.activate
+    def test_apply_group_change_create_no_users(self):
+        no_group_found = json.loads(read_file("test/scim/no_user_found.json"))
+        group_created = json.loads(read_file("test/scim/group_created.json"))
+        group = self.find_entity_by_name(Group, ai_researchers_group)
+        self.clear_group_memberships(group)
+        with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
+            rsps.add(responses.GET, "http://localhost:9002/Groups", json=no_group_found, status=200)
+            rsps.add(responses.POST, "http://localhost:9002/Groups", json=group_created, status=201)
+            apply_group_change(group)
+
+    @responses.activate
+    def test_apply_group_change_create_error_response(self):
+        no_group_found = json.loads(read_file("test/scim/no_user_found.json"))
+        group_created = json.loads(read_file("test/scim/group_created.json"))
+        group = self.find_entity_by_name(Group, ai_researchers_group)
+        self.clear_group_memberships(group)
+        with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
+            rsps.add(responses.GET, "http://localhost:9002/Groups", json=no_group_found, status=200)
+            rsps.add(responses.POST, "http://localhost:9002/Groups", json=group_created, status=400)
+            apply_group_change(group)
