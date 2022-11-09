678b9e5e49d01b4244f8e2b159457c3159338a60
Okke Harsta <oharsta@zilverline.com>
2022-11-09 15:28:56 +0100

    More tests

---
+++ server/test/scim/test_scim.py
@@ -0,0 +1,31 @@
+# -*- coding: future_fstrings -*-
+import json
+
+import responses
+
+from server.db.domain import Collaboration, Service
+from server.scim.scim import membership_user_scim_identifiers
+from server.test.abstract_test import AbstractTest
+from server.test.seed import uva_research_name, service_cloud_name, service_ssh_uva_name
+from server.tools import read_file
+
+
+class TestEvents(AbstractTest):
+
+    @responses.activate
+    def test_membership_user_scim_identifiers_no_scim_service(self):
+        service = self.find_entity_by_name(Service, service_ssh_uva_name)
+        identifiers = membership_user_scim_identifiers(service, None)
+        self.assertListEqual([], identifiers)
+
+    @responses.activate
+    def test_membership_user_scim_identifiers_provisioning_error(self):
+        service = self.find_entity_by_name(Service, service_cloud_name)
+        no_user_found = json.loads(read_file("test/scim/no_user_found.json"))
+        collaboration = self.find_entity_by_name(Collaboration, uva_research_name)
+        with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
+            rsps.add(responses.GET, "http://localhost:8080/api/scim_mock/Users", json=no_user_found, status=200)
+            # We mock that all member provisioning give an error response
+            rsps.add(responses.POST, "http://localhost:8080/api/scim_mock/Users", status=400)
+            identifiers = membership_user_scim_identifiers(service, collaboration)
+            self.assertListEqual([], identifiers)
