1ece71060ad17458e13536c5a17e5c8d6c0ec26e
Okke Harsta <oharsta@zilverline.com>
2022-11-13 12:25:06 +0100

    Test org deleted

--- server/scim/events.py
+++ server/scim/events.py
@@ -33,46 +33,45 @@ def broadcast_user_deleted(user: User):
 
 
 @broadcast_endpoint
-def broadcast_organisation_changed(organisation: Organisation):
-    return current_app.executor.submit(apply_organisation_change, current_app, organisation.id)
+def broadcast_organisation_service_added(organisation: Organisation, service: Service):
+    return current_app.executor.submit(apply_organisation_change, current_app, organisation.id, service.id)
+
+
+@broadcast_endpoint
+def broadcast_organisation_service_deleted(organisation: Organisation, service: Service):
+    return current_app.executor.submit(apply_organisation_change, current_app, organisation.id, service.id, True)
 
 
 @broadcast_endpoint
 def broadcast_organisation_deleted(organisation: Organisation):
-    return current_app.executor.submit(apply_organisation_change, current_app, organisation.id, True)
+    return current_app.executor.submit(apply_organisation_change, current_app, organisation.id, None, True)
 
 
 @broadcast_endpoint
 def broadcast_collaboration_changed(collaboration: Collaboration):
-    return apply_collaboration_change( current_app, collaboration.id)
-    # return current_app.executor.submit(apply_collaboration_change, current_app, collaboration.id)
+    return current_app.executor.submit(apply_collaboration_change, current_app, collaboration.id)
 
 
 @broadcast_endpoint
 def broadcast_collaboration_deleted(collaboration: Collaboration):
-    return apply_collaboration_change( current_app, collaboration.id, True)
-    # return current_app.executor.submit(apply_collaboration_change, current_app, collaboration.id, True)
+    return current_app.executor.submit(apply_collaboration_change, current_app, collaboration.id, True)
 
 
 @broadcast_endpoint
 def broadcast_group_changed(group: Group):
-    apply_group_change( current_app, group.id)
-    # return current_app.executor.submit(apply_group_change, current_app, group.id)
+    return current_app.executor.submit(apply_group_change, current_app, group.id)
 
 
 @broadcast_endpoint
 def broadcast_group_deleted(group: Group):
-    return apply_group_change( current_app, group.id, True)
-    # return current_app.executor.submit(apply_group_change, current_app, group.id, True)
+    return current_app.executor.submit(apply_group_change, current_app, group.id, True)
 
 
 @broadcast_endpoint
 def broadcast_service_added(collaboration: Collaboration, service: Service):
-    return apply_service_changed( current_app, collaboration.id, service.id)
-    # return current_app.executor.submit(apply_service_changed, current_app, collaboration.id, service.id)
+    return current_app.executor.submit(apply_service_changed, current_app, collaboration.id, service.id)
 
 
 @broadcast_endpoint
 def broadcast_service_deleted(collaboration: Collaboration, service: Service):
-    return apply_service_changed( current_app, collaboration.id, service.id, True)
-    # return current_app.executor.submit(apply_service_changed, current_app, collaboration.id, service.id, True)
+    return current_app.executor.submit(apply_service_changed, current_app, collaboration.id, service.id, True)
