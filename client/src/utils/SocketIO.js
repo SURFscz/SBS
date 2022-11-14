3896ac3cb8531c1b193b2540998540afa4c3b68a
Okke Harsta <oharsta@zilverline.com>
2022-11-14 14:29:07 +0100

    Diable socket.io client side
    (cherry picked from commit 9247001203c1b56f6be002bc4c2b974ad179fcff)

--- client/src/utils/SocketIO.js
+++ client/src/utils/SocketIO.js
@@ -1,17 +1,4 @@
-import {io} from "socket.io-client";
-import {config} from "../api";
-
 let initializedSocket = null;
 
-export const socket = initializedSocket ? Promise.resolve(initializedSocket) :
-    config().then(res => {
-        initializedSocket = io(res.socket_url, {
-            transports: ["websocket"],
-            cors: {
-                origin: `${window.location.protocol}//${window.location.host}/`,
-            },
-        });
-        return initializedSocket;
-    });
-
+export const socket = initializedSocket ? Promise.resolve(initializedSocket) : Promise.resolve({on: () => true})
 export const subscriptionIdCookieName = "subscription_id";
