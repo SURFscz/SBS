fec5bc22686f2720468fffb4a0e9bbe2c4db5ac0
Okke Harsta <oharsta@zilverline.com>
2022-11-14 16:03:16 +0100

    More leniency in URL accepting

--- client/src/__tests__/validations/regExp.test.js
+++ client/src/__tests__/validations/regExp.test.js
@@ -1,4 +1,4 @@
-import {sanitizeShortName} from "../../validations/regExps";
+import {sanitizeShortName, validUrlRegExp} from "../../validations/regExps";
 
 test("Sanitize short names", () => {
     expect(sanitizeShortName(null)).toEqual(null);
@@ -7,4 +7,15 @@ test("Sanitize short names", () => {
     expect(sanitizeShortName("&X")).toEqual("x");
     expect(sanitizeShortName("1ABC!D@E#F&G(HIJ)KLMNOPQRSTUVWYZ")).toEqual("abcdefghijklmnop");
     expect(sanitizeShortName("$ABC!D@E#F&G(HIJ)KLMNOPQRSTUVWYZ")).toEqual("abcdefghijklmnop");
+});
+
+test("Valid urls", () => {
+    expect(validUrlRegExp.test("https://localhost/api/scim_mock")).toEqual(true);
+    expect(validUrlRegExp.test("http://localhost/api/scim_mock")).toEqual(true);
+    expect(validUrlRegExp.test("https://demo-sp.sram.surf.nl/test")).toEqual(true);
+
+    expect(validUrlRegExp.test("https://google.nl")).toEqual(true);
+    expect(validUrlRegExp.test("https://google")).toEqual(true);
+
+    expect(validUrlRegExp.test("nope")).toEqual(false);
 });
