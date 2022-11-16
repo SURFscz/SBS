2d2532d5a4ed1d7df72e9f175834bdb1ffb5a50c
Harry Kodden <harry@e-tunity.nl>
2022-11-16 15:05:09 +0100

    Move scim schema to server/scim

---
+++ server/scim/schema_template.py
@@ -0,0 +1,211 @@
+# -*- coding: future_fstrings -*-
+
+SCHEMA_CORE = "urn:ietf:params:scim:schemas:core:2.0"
+
+
+def schema_template(name, schema, attributes):
+    return {
+        "schemas": [
+            f"{SCHEMA_CORE}:Schema"
+        ],
+        "id": schema,
+        "meta": {
+            "resourceType": "Schema",
+            "location": f"/Schemas/{schema}"
+        },
+        "name": name,
+        "Description": f"Defined attributes for the {name} schema",
+        "attributes": attributes
+    }
+
+
+def schema_user_template():
+    return schema_template("User", f"{SCHEMA_CORE}:User", [
+        {
+            "name": "userName",
+            "type": "string",
+            "multiValued": False,
+            "required": True,
+            "caseExact": False,
+            "mutability": "readOnly",
+            "returned": "default",
+            "uniqueness": "server"
+        },
+        {
+            "name": "name",
+            "type": "complex",
+            "multiValued": False,
+            "required": False,
+            "caseExact": False,
+            "mutability": "readOnly",
+            "returned": "default",
+            "uniqueness": "none",
+            "subAttributes": [
+                {
+                    "name": "familyName",
+                    "type": "string",
+                    "multiValued": False,
+                    "required": False,
+                    "caseExact": False,
+                    "mutability": "readOnly",
+                    "returned": "default",
+                    "uniqueness": "none"
+                },
+                {
+                    "name": "givenName",
+                    "type": "string",
+                    "multiValued": False,
+                    "required": False,
+                    "caseExact": False,
+                    "mutability": "readOnly",
+                    "returned": "default",
+                    "uniqueness": "none"
+                }
+            ]
+        },
+        {
+            "name": "displayName",
+            "type": "string",
+            "multiValued": False,
+            "required": False,
+            "caseExact": False,
+            "mutability": "readOnly",
+            "returned": "default",
+            "uniqueness": "none"
+        },
+        {
+            "name": "active",
+            "type": "boolean",
+            "multiValued": False,
+            "required": False,
+            "caseExact": False,
+            "mutability": "readOnly",
+            "returned": "default",
+            "uniqueness": "none"
+        },
+        {
+            "name": "emails",
+            "type": "complex",
+            "multiValued": False,
+            "required": False,
+            "caseExact": False,
+            "mutability": "readOnly",
+            "returned": "default",
+            "uniqueness": "none",
+            "subAttributes": [
+                {
+                    "name": "value",
+                    "type": "string",
+                    "multiValued": False,
+                    "required": False,
+                    "caseExact": False,
+                    "mutability": "readOnly",
+                    "returned": "default",
+                    "uniqueness": "none"
+                },
+                {
+                    "name": "primary",
+                    "type": "boolean",
+                    "multiValued": False,
+                    "required": False,
+                    "caseExact": False,
+                    "mutability": "readOnly",
+                    "returned": "default",
+                    "uniqueness": "none"
+                }
+            ]
+        },
+        {
+            "name": "x509Certificates",
+            "type": "complex",
+            "multiValued": True,
+            "required": False,
+            "caseExact": False,
+            "mutability": "readOnly",
+            "returned": "default",
+            "uniqueness": "none",
+            "subAttributes": [
+                {
+                    "name": "value",
+                    "type": "binary",
+                    "multiValued": False,
+                    "required": False,
+                    "caseExact": False,
+                    "mutability": "readOnly",
+                    "returned": "default",
+                    "uniqueness": "none"
+                }
+            ]
+        }
+    ])
+
+
+def schema_group_template():
+    return schema_template("Group", f"{SCHEMA_CORE}:Group", [
+        {
+            "name": "displayName",
+            "type": "string",
+            "multiValued": False,
+            "required": False,
+            "caseExact": False,
+            "mutability": "readOnly",
+            "returned": "default",
+            "uniqueness": "none"
+        },
+        {
+            "name": "members",
+            "type": "complex",
+            "multiValued": False,
+            "required": False,
+            "caseExact": False,
+            "mutability": "readOnly",
+            "returned": "default",
+            "uniqueness": "none",
+            "subAttributes": [
+                {
+                    "name": "value",
+                    "type": "string",
+                    "multiValued": False,
+                    "required": False,
+                    "caseExact": False,
+                    "mutability": "readOnly",
+                    "returned": "default",
+                    "uniqueness": "none"
+                },
+                {
+                    "name": "$ref",
+                    "type": "reference",
+                    "multiValued": False,
+                    "required": False,
+                    "caseExact": False,
+                    "mutability": "immutable",
+                    "returned": "default",
+                    "uniqueness": "none"
+                },
+                {
+                    "name": "display",
+                    "type": "string",
+                    "multiValued": False,
+                    "required": False,
+                    "caseExact": False,
+                    "mutability": "readOnly",
+                    "returned": "default",
+                    "uniqueness": "none"
+                }
+            ]
+        }
+    ])
+
+
+def schemas_template():
+    schemas = [schema_user_template(), schema_group_template()]
+
+    return {
+          "schemas": [
+              "urn:ietf:params:scim:api:messages:2.0:ListResponse"
+          ],
+          "totalResults": len(schemas),
+          "startIndex": 1,
+          "itemsPerPage": len(schemas),
+          "Resources": schemas
+    }
