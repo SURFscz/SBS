532cf25b826dab406ef50cccc382b9bc62e7427d
Harry Kodden <harry@e-tunity.nl>
2022-11-17 14:01:01 +0100

    adjust test to reference actual base_url

--- server/test/api/test_mfa.py
+++ server/test/api/test_mfa.py
@@ -29,7 +29,7 @@ class TestMfa(AbstractTest):
 
         res = self.post("/api/mfa/verify2fa", {"totp": totp.now()}, with_basic_auth=False)
         self.assertFalse(res["in_proxy_flow"])
-        self.assertEqual("http://localhost:3000", res["location"])
+        self.assertEqual(self.app.app_config.base_url, res["location"])
 
         # Coverage where there is already a second_factor_auth
         self.post("/api/mfa/verify2fa", {"totp": totp.now()}, with_basic_auth=False)
@@ -67,7 +67,7 @@ class TestMfa(AbstractTest):
 
         res = self.get(f"/api/mfa/ssid_start/{sarah.second_fa_uuid}", query_data={"continue_url": "https://foo.bar"},
                        response_status_code=302)
-        self.assertEqual("http://localhost:3000/2fa", res.headers["Location"])
+        self.assertEqual(self.app.app_config.base_url + "/2fa", res.headers["Location"])
 
     def test_2fa_invalid_totp(self):
         AbstractTest.set_second_factor_auth("urn:mary")
@@ -108,7 +108,7 @@ class TestMfa(AbstractTest):
         access_token = self.sign_jwt({"sub": "urn:mary", "kid": "test"})
         res = self.get("/api/mfa/sfo", query_data={"access_token": access_token}, response_status_code=302,
                        with_basic_auth=False)
-        self.assertEqual("http://localhost:3000/2fa", res.headers.get("Location"))
+        self.assertEqual(self.app.app_config.base_url + "/2fa", res.headers.get("Location"))
 
         res = self.get("/api/mfa/get2fa")
         secret = res["secret"]
