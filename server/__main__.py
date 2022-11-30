0294455e8f10532e8f4974edf7505b1745f3033a
Okke Harsta <oharsta@zilverline.com>
2022-11-30 12:26:19 +0100

    Try to connect with redis over ssl

--- server/__main__.py
+++ server/__main__.py
@@ -210,8 +210,12 @@ if not test:
 if not test:
     start_scheduling(app)
 
-# Arguments for debug information
-socket_io = SocketIO(app, message_queue=f"redis://{config.redis.host}:{config.redis.port}", cors_allowed_origins="*")
+redis_password = config.redis.password
+redis_protocol = "rediss" if redis_password else "redis"
+redis_password_part = f"{redis_password}@" if redis_password else ""
+socket_io = SocketIO(app,
+                     message_queue=f"{redis_protocol}://{redis_password_part}{config.redis.host}:{config.redis.port}",
+                     cors_allowed_origins="*")
 app.socket_io = socket_io
 
 
