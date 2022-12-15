a72709f982bebcdf15641672a710e52ba20678ae
Okke Harsta <oharsta@zilverline.com>
2022-12-15 12:05:15 +0100

    Fixed tests

--- server/db/models.py
+++ server/db/models.py
@@ -20,7 +20,8 @@ deserialization_mapping = {"users": User, "collaboration_memberships": Collabora
 
 forbidden_fields = ["created_at", "updated_at"]
 date_fields = ["start_date", "end_date", "created_at", "updated_at", "last_accessed_date", "last_login_date",
-               "last_activity_date", "membership_expiry_date", "expiry_date", "invitation_expiry_date"]
+               "last_activity_date", "membership_expiry_date", "expiry_date", "invitation_expiry_date",
+               "sweep_scim_last_run"]
 
 
 def flatten(coll):
