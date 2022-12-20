903d2ce7376c9caf516fc976c566d8d0572b4413
Okke Harsta <oharsta@zilverline.com>
2022-12-20 13:36:17 +0100

    Wordings

--- client/src/locale/en.js
+++ client/src/locale/en.js
@@ -1736,7 +1736,7 @@ I18n.translations.en = {
         scim: {
             info: "If enabled there is a mock remote SCIM client available to test the SCIM messages." +
                 "Configure a Service with the following SCIM URL to start testing the SCIM functionality" ,
-            services: "These services are currently configured with SCIM enabled and the SCIM mock endpoint",
+            services: "SCIM enabled Services",
             stats: "All API calls and state updates send to the mock SCIM endpoints are stored in an in-memory database.",
             retrieveStats: "Fetch stats",
             reRetrieveStats: "Refresh stats",
