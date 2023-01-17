f635e730547b8e0e2c323d5a9ae208878dcf1e44
Okke Harsta <oharsta@zilverline.com>
2023-01-17 13:09:53 +0100

    Bugfix for empty mine services

--- server/test/api/test_service.py
+++ server/test/api/test_service.py
@@ -275,6 +275,11 @@ class TestService(AbstractTest):
         self.assertEqual(0, service_network["organisations_count"])
         self.assertEqual(1, service_network["collaborations_count"])
 
+    def test_services_mine_bug(self):
+        self.login("urn:jane")
+        services = self.get("/api/services/mine", with_basic_auth=False)
+        self.assertEqual(0, len(services))
+
     def test_services_all_org_member(self):
         self.login("urn:harry")
         services = self.get("/api/services/all", with_basic_auth=False)
