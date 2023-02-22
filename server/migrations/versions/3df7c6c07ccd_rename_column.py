00a29ffb6f45d5e15bcbad4101ad4e4409f75a71
Okke Harsta <oharsta@zilverline.com>
2023-02-22 16:11:19 +0100

    Flake8

--- server/migrations/versions/3df7c6c07ccd_rename_column.py
+++ server/migrations/versions/3df7c6c07ccd_rename_column.py
@@ -6,9 +6,7 @@ Create Date: 2023-02-22 14:15:21.107086
 
 """
 from alembic import op
-import sqlalchemy as sa
 from sqlalchemy import text
-
 
 # revision identifiers, used by Alembic.
 revision = '3df7c6c07ccd'
