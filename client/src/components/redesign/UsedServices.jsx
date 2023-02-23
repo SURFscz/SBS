7049625ab53b423c663c18b976df35eb633a9453
Okke Harsta <oharsta@zilverline.com>
2023-02-23 14:04:46 +0100

    Collaborations can connect to services when the organisation is trusted

--- client/src/components/redesign/UsedServices.jsx
+++ client/src/components/redesign/UsedServices.jsx
@@ -76,7 +76,9 @@ class UsedServices extends React.Component {
                 const filteredServices = services
                     .filter(service => {
                         return (service.allowed_organisations.some(org => org.id === collaboration.organisation_id)
-                                || service.access_allowed_for_all) && servicesInUse.indexOf(service.id) === -1
+                                || service.access_allowed_for_all
+                                || service.automatic_connection_allowed_organisations.some(org => org.id === collaboration.organisation_id))
+                            && servicesInUse.indexOf(service.id) === -1
                             && (service.allow_restricted_orgs || !collaboration.organisation.services_restricted);
                     });
                 this.setState({services: filteredServices, loading: false});
