2223da8ddf229dab5fb0b226df5d7f83f89c2fdf
Okke Harsta <oharsta@zilverline.com>
2023-03-07 11:37:00 +0100

    Fixes #529

--- server/api/dynamic_extended_json_provider.py
+++ server/api/dynamic_extended_json_provider.py
@@ -1,13 +1,13 @@
 import time
 from datetime import date
 
-from flask.json import JSONEncoder
+from flask.json.provider import DefaultJSONProvider
 
 
-class DynamicExtendedJSONEncoder(JSONEncoder):
+class DynamicExtendedJSONProvider(DefaultJSONProvider):
     def default(self, o):
         if hasattr(o, "__json__"):
             return o.__json__()
         if isinstance(o, date):
             return int(time.mktime(o.timetuple()))
-        return super(DynamicExtendedJSONEncoder, self).default(o)
+        return super(DynamicExtendedJSONProvider, self).default(o)
