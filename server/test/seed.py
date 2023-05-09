c1a7ccc6eebd04c05a8396ceaa0d531493569716
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2023-05-09 14:38:18 +0200

    set ldap password for cloud service in seed

--- server/test/seed.py
+++ server/test/seed.py
@@ -305,11 +305,14 @@ def seed(db, app_config, skip_seed=False, perf_test=False):
                        logo=read_image("wireless.png"), accepted_user_policy="https://google.nl", abbreviation="wire",
                        allowed_organisations=[uuc, uva], uri="https://wireless", non_member_users_access_allowed=True,
                        privacy_policy="https://privacy.org", security_email="sec@org.nl")
+    # ldap_password is 'changethispassword'
     cloud = Service(entity_id=service_cloud_entity_id, name=service_cloud_name, description="SARA Cloud Service",
                     public_visible=True, automatic_connection_allowed=True, logo=read_image("cloud.jpg"),
                     allowed_organisations=[uuc, uva], abbreviation="cloud", privacy_policy="https://privacy.org",
                     token_enabled=True, token_validity_days=1, security_email="sec@org.nl", scim_client_enabled=True,
-                    scim_enabled=True, scim_url="http://localhost:8080/api/scim_mock", scim_bearer_token="secret")
+                    scim_enabled=True, scim_url="http://localhost:8080/api/scim_mock", scim_bearer_token="secret",
+                    ldap_password="$6$rounds=656000$C7wjC1/EcEGz1DFv$ouYWXyzdirP1ToXQCtR7ZyS4Fe1vGp.1rF6WWA3wKdb"
+                                  "fuqVpBngV1xjMWmpwSZh8DRVO.9WxhT.StwNMRqCwe0")
     storage = Service(entity_id=service_storage_entity_id, name=service_storage_name, allowed_organisations=[uuc, uva],
                       description="SURF Storage Service", logo=read_image("storage.jpeg"), abbreviation="storage",
                       public_visible=True, automatic_connection_allowed=True, allow_restricted_orgs=True,
