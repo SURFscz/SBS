8f380de9bd478f97497437da6d42c962b6305820
Okke Harsta <oharsta@zilverline.com>
2023-05-22 15:08:56 +0200

    Fixes #727

--- client/src/components/redesign/ClipBoardCopy.jsx
+++ client/src/components/redesign/ClipBoardCopy.jsx
@@ -5,15 +5,17 @@ import {CopyToClipboard} from "react-copy-to-clipboard";
 
 export default function ClipBoardCopy({txt, right = false, transparentBackground = false}) {
     return (
-        <section className={`copy-to-clipboard ${right ? "right" : ""} ${transparentBackground ? "transparent" : ""}`}>
-            <CopyToClipboard text={txt}>
-                <Duplicate onClick={e => {
+        <CopyToClipboard text={txt}>
+            <section
+                className={`copy-to-clipboard ${right ? "right" : ""} ${transparentBackground ? "transparent" : ""}`}
+                onClick={e => {
                     const me = e.target;
                     me.classList.add("copied");
                     setTimeout(() => me.classList.remove("copied"), 1250);
-                }}/>
-            </CopyToClipboard>
-        </section>
+                }}>
+                <Duplicate/>
+            </section>
+        </CopyToClipboard>
     );
 
 }
