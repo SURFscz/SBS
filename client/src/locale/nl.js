4cef5876e43c689cb1b1750b39981db9bddc57b5
Floris Fokkinga <80269262+FlorisFokkinga@users.noreply.github.com>
2023-05-24 14:23:44 +0200

    Update translation: missing Dutch

--- client/src/locale/nl.js
+++ client/src/locale/nl.js
@@ -1846,7 +1846,7 @@ const nl = {
     },
     welcomeDialog: {
         title: "Welkom bij {{name}}",
-        hi: "Hi,",
+        hi: "Hoi,",
         label: "",
         roleServiceAdmin: "Je bent uitgenodigd om <strong>dienstbeheerder</strong> te worden",
         roleOrganisationAdmin: "Je bent uitgenodigd om <strong>beheerder</strong> te worden",
