c29d98ff70bc540451742b010e1a446afd862d09
Okke Harsta <oharsta@zilverline.com>
2023-09-19 15:09:11 +0200

    Fixes 623#

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -103,7 +103,7 @@ export function me(config) {
     if (config.local && 1 == 1) {
         let sub = "urn:service_admin";
         sub = "urn:john";
-        sub = "urn:james";
+        // sub = "urn:james";
         const second_factor_confirmed = true;
         // sub = "urn:unknown";
         // Need to mock a login
