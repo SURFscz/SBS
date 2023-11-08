687c6a8bbdd9fe3cfacf11058c2647a3d6495439
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2023-11-08 16:46:29 +0100

    try to fix CSP

--- client/src/utils/IconLibrary.js
+++ client/src/utils/IconLibrary.js
@@ -1,4 +1,10 @@
-import {library} from '@fortawesome/fontawesome-svg-core'
+import {library, config} from '@fortawesome/fontawesome-svg-core'
+
+// Make sure FontAwesome doesn't use inline CSS styles
+// see https://fontawesome.com/docs/web/dig-deeper/security#content-security-policy
+// Make sure this is before any other `fontawesome` API calls
+config.autoAddCss = false;
+
 import {
     faAngleDown,
     faAngleUp,
