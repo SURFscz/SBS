6c3be4023e00de3b537b804ffaaf237175bfa7eb
Okke Harsta <oharsta@zilverline.com>
2023-12-22 16:26:16 +0100

    WIP for #1122

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -104,8 +104,8 @@ export function me(config) {
         let sub = "urn:service_admin";
         sub = "urn:john";
         //sub = "urn:james";
-        // const second_factor_confirmed = true;
-        const second_factor_confirmed = false;
+        const second_factor_confirmed = true;
+        // const second_factor_confirmed = false;
         // sub = "urn:unknown";
         // Need to mock a login
         return postPutJson("/api/mock", {
