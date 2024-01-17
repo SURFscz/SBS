68f1278c6cf0cb8b826597e734f8b07926323e6d
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2024-01-17 16:04:54 +0100

    increase coverage and fix bug in shortname generation found by new test

--- server/test/db/test_domain.py
+++ server/test/db/test_domain.py
@@ -1,16 +1,19 @@
 from sqlalchemy.exc import IntegrityError
 
 from server.db.db import db
-from server.db.domain import Collaboration, CollaborationMembership, Invitation, OrganisationInvitation
+from server.db.domain import Collaboration, CollaborationMembership, Invitation, OrganisationInvitation, User
 from server.test.abstract_test import AbstractTest
-from server.test.seed import co_ai_computing_name
+from server.test.seed import co_ai_computing_name, user_boss_name
 
 
 class TestModels(AbstractTest):
 
     def test_collaboration(self):
         collaboration = self.find_entity_by_name(Collaboration, co_ai_computing_name)
-        self.assertEqual(False, collaboration.is_admin(999))
+        self.assertFalse(collaboration.is_admin(999))
+
+        admin = self.find_entity_by_name(User, user_boss_name)
+        self.assertTrue(collaboration.is_admin(admin.id))
 
     def test_invitation_role(self):
         Invitation.validate_role("admin")
