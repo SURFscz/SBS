bf45d464b46930d50b1cd67e2b80f1b806620871
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2024-01-24 17:10:30 +0100

    automatically create databases

--- server/test/conftest.py
+++ server/test/conftest.py
@@ -4,10 +4,15 @@
 
 import logging
 import os
+import re
 
 import pytest
+import yaml
+from munch import munchify
 
 from server.db.db import db_migrations
+from server.tools import read_file
+
 
 def pytest_sessionstart(session):
     print("Starting pytest session...")
@@ -21,9 +26,26 @@ def pytest_sessionstart(session):
 @pytest.fixture(autouse=True, scope='session')
 def use_random_db(request, worker_id):
     logging.error(f"random_db fixture called: {worker_id}")
-    with open(f'/tmp/pytest_{worker_id}', 'a') as f:
-        f.write(worker_id)
 
-    os.environ['SBS_DB_NAME_OVERRIDE'] = f"mysql+mysqldb://sbs:sbs@127.0.0.1/sbs_{worker_id}?charset=utf8mb4"
-    db_migrations(os.environ['SBS_DB_NAME_OVERRIDE'])
+    if worker_id == "master":
+        return
+
+    # make sure we use a separate DB for each worker
+    # and make sure the database is created and migrated
+
+    config_file = os.environ.get("CONFIG", "config/test_config.yml")
+    config = munchify(yaml.load(read_file(config_file), Loader=yaml.FullLoader))
+    database_uri_root = re.sub(r"/sbs_test\b", "/", config.database.uri)
+    database_uri = re.sub(r"/sbs_test\b", f"/sbs_{worker_id}", config.database.uri)
+
+    with open(f'/tmp/pytest_{worker_id}', 'a') as f:
+        f.write(f"{worker_id}\n{database_uri}\n")
+
+    import sqlalchemy
+    engine = sqlalchemy.create_engine(database_uri_root)
+    with engine.connect() as conn:
+        conn.execute(sqlalchemy.text(f"CREATE DATABASE IF NOT EXISTS sbs_{worker_id}"))
+
+    os.environ['SBS_DB_NAME_OVERRIDE'] = database_uri
+    db_migrations(database_uri)
 
