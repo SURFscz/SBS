97a2e817b64f1c56b4392f937ee5aa2203ca18b8
Okke Harsta <oharsta@zilverline.com>
2024-02-28 17:17:26 +0100

    WIP for #498

--- server/auth/secrets.py
+++ server/auth/secrets.py
@@ -1,9 +1,13 @@
+import base64
 import hashlib
+import json
+import secrets
 import string
 from secrets import token_urlsafe, SystemRandom
 
 import bcrypt
-from werkzeug.exceptions import SecurityError
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from werkzeug.exceptions import SecurityError, BadRequest
 
 SYSTEM_RANDOM = SystemRandom()
 
@@ -32,3 +36,28 @@ def generate_ldap_password_with_hash():
     password = start + "".join(SYSTEM_RANDOM.sample(population=ldap_characters, k=31))
     hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     return hashed.decode("utf-8"), password
+
+
+def encrypt_secret(encryption_key: str, plain_secret, context: dict) -> str:
+    nonce = secrets.token_urlsafe()
+    context["plain_secret"] = plain_secret
+    base64_encoded_context = {key: base64.b64encode(str(value).encode()).decode() for (key, value) in context.items()}
+    aes_gcm = AESGCM(base64.b64decode(encryption_key))
+    data = json.dumps(base64_encoded_context).encode()
+    encrypted_context = aes_gcm.encrypt(nonce.encode(), data, None)
+    return f"{nonce}:{base64.b64encode(encrypted_context).decode()}"
+
+
+def decrypt_secret(encryption_key: str, encrypted_value: str, context: dict) -> str:
+    aes_gcm = AESGCM(base64.b64decode(encryption_key))
+    index = encrypted_value.index(':')
+    nonce = encrypted_value[0:index]
+    encrypted_context = encrypted_value[index + 1:]
+    data = base64.b64decode(encrypted_context)
+    decrypted = aes_gcm.decrypt(nonce.encode(), data, None).decode()
+    base64_context = json.loads(decrypted)
+    decrypted_context = {key: base64.b64decode(value.encode()).decode() for (key, value) in base64_context.items()}
+    for key, value in context.items():
+        if str(value) != decrypted_context[key]:
+            raise BadRequest(f"Invalid value(={decrypted_context[key]}) for {key}, expected {value}")
+    return decrypted_context["plain_secret"]
--- server/test/auth/test_secrets.py
+++ server/test/auth/test_secrets.py
@@ -1,6 +1,10 @@
+import base64
 from unittest import TestCase
 
-from server.auth.secrets import secure_hash, generate_token, generate_ldap_password_with_hash
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+from server.auth.secrets import secure_hash, generate_token, generate_ldap_password_with_hash, decrypt_secret, \
+    encrypt_secret
 
 
 class TestSecret(TestCase):
@@ -14,3 +18,17 @@ class TestSecret(TestCase):
     def test_ldap_password(self):
         _, password = generate_ldap_password_with_hash()
         self.assertEqual(32, len(password))
+
+    def test_encrypt_decrypt_secret(self):
+        encryption_key = base64.b64encode(AESGCM.generate_key(bit_length=256)).decode()
+        context = {
+            "database_name": "sbs_test",
+            "table_name": "services",
+            "identifier": 999,
+            "scim_url": "https://scim.url"
+        }
+        plain_secret = "https://top_secret.com?query=params"
+        encrypted_value = encrypt_secret(encryption_key, plain_secret, context)
+
+        decrypted_secret = decrypt_secret(encryption_key, encrypted_value, context)
+        self.assertEqual(plain_secret, decrypted_secret)
