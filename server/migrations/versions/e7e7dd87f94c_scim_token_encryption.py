15581c1951c11daf1e6bb1b9c681403b3e2e92bf
Okke Harsta <oharsta@zilverline.com>
2024-02-29 15:42:58 +0100

    Fix for #498 migration

--- server/migrations/versions/e7e7dd87f94c_scim_token_encryption.py
+++ server/migrations/versions/e7e7dd87f94c_scim_token_encryption.py
@@ -13,7 +13,6 @@ from munch import munchify
 from sqlalchemy import text
 
 from server.auth.secrets import encrypt_secret
-from server.tools import read_file
 
 # revision identifiers, used by Alembic.
 revision = 'e7e7dd87f94c'
@@ -24,23 +23,25 @@ depends_on = None
 
 def upgrade():
     config_file_location = os.environ.get("CONFIG", "config/config.yml")
-    config = munchify(yaml.load(read_file(config_file_location), Loader=yaml.FullLoader))
-    encryption_key = config.encryption_key
-    conn = op.get_bind()
-    result = conn.execute(text("SELECT id, scim_url, scim_bearer_token FROM services WHERE scim_url IS NOT NULL "
-                               "AND scim_bearer_token IS NOT NULL"))
-    for row in result:
-        identifier = row[0]
-        scim_url = row[1]
-        scim_bearer_token = row[2]
-        context = {
-            "scim_url": scim_url,
-            "identifier": identifier,
-            "table_name": "services"
-        }
-        secret = encrypt_secret(encryption_key, scim_bearer_token, context)
-        conn.execute(text("UPDATE services SET scim_bearer_token = :scim_bearer_token WHERE id = :id"),
-                     dict(scim_bearer_token=secret, id=identifier))
+    file = f"{os.path.dirname(os.path.realpath(__file__))}/../../{config_file_location}"
+    with open(file) as f:
+        config = munchify(yaml.load(f.read(), Loader=yaml.FullLoader))
+        encryption_key = config.encryption_key
+        conn = op.get_bind()
+        result = conn.execute(text("SELECT id, scim_url, scim_bearer_token FROM services WHERE scim_url IS NOT NULL "
+                                   "AND scim_bearer_token IS NOT NULL"))
+        for row in result:
+            identifier = row[0]
+            scim_url = row[1]
+            scim_bearer_token = row[2]
+            context = {
+                "scim_url": scim_url,
+                "identifier": identifier,
+                "table_name": "services"
+            }
+            secret = encrypt_secret(encryption_key, scim_bearer_token, context)
+            conn.execute(text("UPDATE services SET scim_bearer_token = :scim_bearer_token WHERE id = :id"),
+                         dict(scim_bearer_token=secret, id=identifier))
 
 
 def downgrade():
