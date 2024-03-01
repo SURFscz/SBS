4ba263ce68e8782a7a38c1c27c6a50602764eebf
Okke Harsta <oharsta@zilverline.com>
2024-03-01 09:40:42 +0100

    Refactored SCIM bearer token migration

--- server/migrations/versions/e7e7dd87f94c_scim_token_encryption.py
+++ server/migrations/versions/e7e7dd87f94c_scim_token_encryption.py
@@ -5,20 +5,31 @@ Revises: bfff5ff0d8e6
 Create Date: 2024-02-29 09:07:10.144305
 
 """
+import base64
+import json
 import os
+import secrets
 
 import yaml
 from alembic import op
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from munch import munchify
 from sqlalchemy import text
-
-from server.auth.secrets import encrypt_secret
 
 # revision identifiers, used by Alembic.
 revision = 'e7e7dd87f94c'
 down_revision = 'bfff5ff0d8e6'
 branch_labels = None
 depends_on = None
+
+
+def encrypt_secret(encryption_key: str, plain_secret: str, context: dict) -> str:
+    nonce = secrets.token_urlsafe()
+    context["plain_secret"] = plain_secret
+    aes_gcm = AESGCM(base64.b64decode(encryption_key))
+    data = json.dumps(context).encode()
+    encrypted_context = aes_gcm.encrypt(nonce.encode(), data, None)
+    return f"{nonce}:{base64.b64encode(encrypted_context).decode()}"
 
 
 def upgrade():
