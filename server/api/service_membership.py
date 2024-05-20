fedb92beb79d12d29bd519aa52f3468d145524ae
Okke Harsta <oharsta@zilverline.com>
2024-05-20 08:37:36 +0200

    Fixes #1410

--- server/api/service_membership.py
+++ server/api/service_membership.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request as current_request
+from werkzeug.exceptions import Forbidden
 
 from server.api.base import json_endpoint, emit_socket
 from server.auth.security import confirm_service_admin, confirm_write_access, current_user_id, current_user_uid
@@ -12,18 +13,22 @@ service_membership_api = Blueprint("service_membership_api", __name__,
 @service_membership_api.route("/<service_id>/<user_id>", methods=["DELETE"], strict_slashes=False)
 @json_endpoint
 def delete_service_membership(service_id, user_id):
-    confirm_service_admin(service_id)
+    try:
+        confirm_service_admin(service_id)
+    except Forbidden as e:
+        # A Service Manager may delete its own membership
+        if current_user_id() != int(user_id):
+            raise e
 
-    memberships = ServiceMembership.query \
+    membership = ServiceMembership.query \
         .filter(ServiceMembership.service_id == service_id) \
         .filter(ServiceMembership.user_id == user_id) \
-        .all()
-    for membership in memberships:
-        db.session.delete(membership)
+        .one()
+    db.session.delete(membership)
 
     emit_socket(f"service_{service_id}", include_current_user_id=True)
 
-    return (None, 204) if len(memberships) > 0 else (None, 404)
+    return None, 204
 
 
 @service_membership_api.route("/", methods=["POST"], strict_slashes=False)
