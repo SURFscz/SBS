c5e9e6450d890d1a5c6743b89a828677e90f0a4b
Okke Harsta <oharsta@zilverline.com>
2024-05-23 12:56:19 +0200

    Obtain lock for generating username

---
+++ server/migrations/versions/c63acd12d7ea_dummy.py
@@ -0,0 +1,21 @@
+"""dummy
+
+Revision ID: c63acd12d7ea
+Revises: 65fbafbc2ce8
+Create Date: 2024-05-23 12:52:15.511002
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'c63acd12d7ea'
+down_revision = '65fbafbc2ce8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
