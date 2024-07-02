56492d80edf0a9deae075beca60c39620acb7d23
Okke Harsta <oharsta@zilverline.com>
2024-07-02 08:54:38 +0200

    Fixed tests

--- server/test/api/test_system.py
+++ server/test/api/test_system.py
@@ -148,9 +148,9 @@ class TestSystem(AbstractTest):
     def test_parse_metadata(self):
         res = self.get("/api/system/parse_metadata")
         self.assertIn("entity_ids", res)
-        self.assertEqual(2, len(res["entity_ids"]))
+        self.assertEqual(3, len(res["entity_ids"]))
         self.assertIn("schac_home_organizations", res)
-        self.assertEqual(4, len(res["schac_home_organizations"]))
+        self.assertEqual(5, len(res["schac_home_organizations"]))
 
     def test_sweep(self):
         res = self.get("/api/system/sweep")
