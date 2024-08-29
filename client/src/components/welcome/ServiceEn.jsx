ddd6bd25cab1fa3e05f797301c77c14a4b09f239
Floris Fokkinga <80269262+FlorisFokkinga@users.noreply.github.com>
2024-08-29 16:18:02 +0200

    Update translation service to application

--- client/src/components/welcome/ServiceEn.jsx
+++ client/src/components/welcome/ServiceEn.jsx
@@ -7,10 +7,10 @@ export default function ServiceEn({role}) {
 
     const responsibilities = role === ROLES.SERVICE_ADMIN ? [
             "View and edit its properties",
-            "Configure the service AUP and Privacy Policy",
-            "Manage which organisations and collaborations can use this service",
+            "Configure the application AUP and Privacy Policy",
+            "Manage which organisations and collaborations can use this application",
             "View LDAP information and set the bind password",
-            "Manage other service admins for this service"
+            "Manage other application admins for this application"
         ] :
         [
             "Manage connection requests for the Service",
@@ -19,7 +19,7 @@ export default function ServiceEn({role}) {
     return (
         <div className="welcome">
             <div>
-                <p>As a service {role === ROLES.SERVICE_ADMIN ? "admin" : "manager"}, you can:</p>
+                <p>As an application {role === ROLES.SERVICE_ADMIN ? "admin" : "manager"}, you can:</p>
                 <ul>
                     {responsibilities.map((r, i) => <li key={i}>{r}</li>)}
                 </ul>
