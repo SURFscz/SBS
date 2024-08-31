3e6a2796b59b34f76be433580c9cbc3e43265c23
Okke Harsta <oharsta@zilverline.com>
2024-08-31 09:02:43 +0200

    Fixes #1203

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./NotFound.scss";
 import {ReactComponent as NotFoundSVG} from "../lotties/undraw_page_not_found.svg";
+import {ReactComponent as InvitationNotFoundSVG} from "../icons/invitation_404.svg";
+
 import I18n from "../locale/I18n";
 import DOMPurify from "dompurify";
 
 export default function NotFound({config}) {
+
     const urlSearchParams = new URLSearchParams(window.location.search);
     const translation = I18n.translations[I18n.locale];
     let errorMessage = DOMPurify.sanitize(urlSearchParams.get("eo") || "msg");
@@ -16,7 +19,8 @@ export default function NotFound({config}) {
         <div className="mod-not-found">
             <div className="mod-inner-not-found">
                 <p className="not-found-msg" dangerouslySetInnerHTML={{__html: html}}/>
-                <NotFoundSVG/>
+                {errorMessage === "invitationNotFound" && <InvitationNotFoundSVG/>}
+                {errorMessage !== "invitationNotFound" && <NotFoundSVG/>}
             </div>
         </div>
     );
