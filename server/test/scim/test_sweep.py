c505d53e015826c678bc143c9122950116611255
Harry Kodden <harry.kodden@surf.nl>
2024-10-02 14:54:08 +0200

    SIM Improvement, issue 1593

--- server/test/scim/test_sweep.py
+++ server/test/scim/test_sweep.py
@@ -5,12 +5,15 @@ from werkzeug.exceptions import BadRequest
 
 from server.api.base import application_base_url
 from server.db.domain import Service, Group, User, Collaboration
+
 from server.scim import SCIM_GROUPS
 from server.scim.group_template import create_group_template, scim_member_object
 from server.scim.repo import all_scim_groups_by_service
 from server.scim.schema_template import SCIM_SCHEMA_SRAM_GROUP
 from server.scim.sweep import perform_sweep, _all_remote_scim_objects, _group_changed, _user_changed
 from server.scim.user_template import create_user_template, find_user_by_id_template
+
+from server.test.scim import TEST_SCIM_SERVER, TEST_SCIM_USERS_ENDPOINT, TEST_SCIM_GROUPS_ENDPOINT
 from server.test.abstract_test import AbstractTest
 from server.test.seed import service_network_name, group_ai_researchers, user_john_name, co_ai_computing_name
 from server.tools import read_file
@@ -28,15 +31,15 @@ class TestSweep(AbstractTest):
         remote_groups = json.loads(read_file("test/scim/sweep/remote_groups_unchanged.json"))
         remote_users = json.loads(read_file("test/scim/sweep/remote_users_unchanged.json"))
         with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
-            rsps.add(responses.GET, "http://localhost:8080/api/scim_mock/Users", json=remote_users, status=200)
-            rsps.add(responses.GET, "http://localhost:8080/api/scim_mock/Groups", json=remote_groups, status=200)
+            rsps.add(responses.GET, TEST_SCIM_USERS_ENDPOINT, json=remote_users, status=200)
+            rsps.add(responses.GET, TEST_SCIM_GROUPS_ENDPOINT, json=remote_groups, status=200)
 
             sync_results = perform_sweep(service)
 
             self.assertDictEqual({
                 "users": {"deleted": [], "created": [], "updated": []},
                 "groups": {"deleted": [], "created": [], "updated": []},
-                "scim_url": "http://localhost:8080/api/scim_mock"
+                "scim_url": TEST_SCIM_SERVER
             }, sync_results)
 
     @responses.activate
@@ -45,27 +48,27 @@ class TestSweep(AbstractTest):
         remote_groups = json.loads(read_file("test/scim/sweep/remote_groups_changes.json"))
         remote_users = json.loads(read_file("test/scim/sweep/remote_users_changes.json"))
         with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
-            rsps.add(responses.GET, "http://localhost:8080/api/scim_mock/Users", json=remote_users, status=200)
-            rsps.add(responses.GET, "http://localhost:8080/api/scim_mock/Groups", json=remote_groups, status=200)
+            rsps.add(responses.GET, TEST_SCIM_USERS_ENDPOINT, json=remote_users, status=200)
+            rsps.add(responses.GET, TEST_SCIM_GROUPS_ENDPOINT, json=remote_groups, status=200)
             # Perform a diff between remote_groups_changes.json and remote_groups_unchanged.json to explain responses
             rsps.add(responses.DELETE,
-                     "http://localhost:8080/api/scim_mock/Groups/19D2F1B9-5A1D-43D5-8F0C-FE6474E313E3",
+                     f"{TEST_SCIM_GROUPS_ENDPOINT}/19D2F1B9-5A1D-43D5-8F0C-FE6474E313E3",
                      status=204)
             rsps.add(responses.DELETE,
-                     "http://localhost:8080/api/scim_mock/Users/1DE53F36-2DB3-4CD4-8204-6692958F1133",
+                     f"{TEST_SCIM_USERS_ENDPOINT}/1DE53F36-2DB3-4CD4-8204-6692958F1133",
                      status=204)
             user_created = json.loads(read_file("test/scim/user_created.json"))
             rsps.add(responses.PUT,
-                     "http://localhost:8080/api/scim_mock/Users/CC7352C1-D752-4588-9FF3-014800EA094B",
+                     f"{TEST_SCIM_USERS_ENDPOINT}/CC7352C1-D752-4588-9FF3-014800EA094B",
                      json=user_created, status=201)
-            rsps.add(responses.POST, "http://localhost:8080/api/scim_mock/Users", json=user_created, status=201)
+            rsps.add(responses.POST, TEST_SCIM_USERS_ENDPOINT, json=user_created, status=201)
 
             group_created = json.loads(read_file("test/scim/group_created.json"))
-            rsps.add(responses.PUT, "http://localhost:8080/api/scim_mock/Groups/768F27CF-98E2-42B4-9913-3AACF370D8FD",
+            rsps.add(responses.PUT, f"{TEST_SCIM_GROUPS_ENDPOINT}/768F27CF-98E2-42B4-9913-3AACF370D8FD",
                      json=group_created, status=201)
-            rsps.add(responses.PUT, "http://localhost:8080/api/scim_mock/Groups/204DD260-D297-41E2-97AF-CDEF68EFB35B",
+            rsps.add(responses.PUT, f"{TEST_SCIM_GROUPS_ENDPOINT}/204DD260-D297-41E2-97AF-CDEF68EFB35B",
                      json=group_created, status=201)
-            rsps.add(responses.POST, "http://localhost:8080/api/scim_mock/Groups", json=group_created, status=201)
+            rsps.add(responses.POST, TEST_SCIM_GROUPS_ENDPOINT, json=group_created, status=201)
 
             sync_results = perform_sweep(service)
             self.assertEqual(1, len(sync_results["users"]["deleted"]))
@@ -87,15 +90,15 @@ class TestSweep(AbstractTest):
             self.save_entity(group)
 
         with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
-            rsps.add(responses.GET, "http://localhost:8080/api/scim_mock/Users", json=remote_users, status=200)
-            rsps.add(responses.GET, "http://localhost:8080/api/scim_mock/Groups", json=remote_groups, status=200)
+            rsps.add(responses.GET, TEST_SCIM_USERS_ENDPOINT, json=remote_users, status=200)
+            rsps.add(responses.GET, TEST_SCIM_GROUPS_ENDPOINT, json=remote_groups, status=200)
             for group_id in [g["id"] for g in remote_groups["Resources"]]:
                 rsps.add(responses.DELETE,
-                         f"http://localhost:8080/api/scim_mock/Groups/{group_id}",
+                         f"{TEST_SCIM_GROUPS_ENDPOINT}/{group_id}",
                          status=204)
             for user_id in [u["id"] for u in remote_users["Resources"]]:
                 rsps.add(responses.DELETE,
-                         f"http://localhost:8080/api/scim_mock/Users/{user_id}",
+                         f"{TEST_SCIM_USERS_ENDPOINT}/{user_id}",
                          status=204)
 
             sync_results = perform_sweep(service)
@@ -112,7 +115,7 @@ class TestSweep(AbstractTest):
         remote_groups = {"totalResults": 3,
                          "Resources": [1]}
         with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
-            rsps.add(responses.GET, "http://localhost:8080/api/scim_mock/Groups", json=remote_groups,
+            rsps.add(responses.GET, TEST_SCIM_GROUPS_ENDPOINT, json=remote_groups,
                      status=200)
             scim_objects = _all_remote_scim_objects(service, SCIM_GROUPS)
             self.assertEqual(3, len(scim_objects))
@@ -122,7 +125,7 @@ class TestSweep(AbstractTest):
         def all_remote():
             service = self.find_entity_by_name(Service, service_network_name)
             with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
-                rsps.add(responses.GET, "http://localhost:8080/api/scim_mock/Groups", json={"error": True},
+                rsps.add(responses.GET, TEST_SCIM_GROUPS_ENDPOINT, json={"error": True},
                          status=400)
                 _all_remote_scim_objects(service, SCIM_GROUPS)
             self.assertRaises(BadRequest, all_remote)
