3df93207e5f1d03a2ff1f14c5fa9a056844afdf6
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2024-11-06 14:43:18 +0100

    allow running migratsions only

--- misc/entrypoint.sh
+++ misc/entrypoint.sh
@@ -46,12 +46,19 @@ cd /opt/sbs
 
 # Run migrations
 _RUN_MIGRATIONS=${RUN_MIGRATIONS:-0}
-if [ "$_RUN_MIGRATIONS" -eq 1 ]; then
+_MIGRATIONS_ONLY=${MIGRATIONS_ONLY:-0}
+if [ "$_RUN_MIGRATIONS" -eq 1 ] || [ "$_MIGRATIONS_ONLY" -eq 1 ]
+then
     echo "Running migrations"
     cd /opt/sbs/server
     ${PRIVDROP} /usr/local/bin/alembic --config /opt/sbs/server/migrations/alembic.ini upgrade head
-fi
 
+    if [ "$_MIGRATIONS_ONLY" -eq 1 ]
+    then
+        echo "Migrations done, exiting"
+        exit 0
+    fi
+fi
 
 # Hand off to the CMD
 exec ${PRIVDROP} $@
