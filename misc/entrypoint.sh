320adf5262d80466e637f3866be403e71f2195e0
Martin van Es <martin@mrvanes.com>
2024-11-18 16:13:26 +0100

    WIP

--- misc/entrypoint.sh
+++ misc/entrypoint.sh
@@ -15,10 +15,10 @@ rm -f /opt/sbs/server/config/config.yml
 rm -f /opt/sbs/server/migrations/alembic.ini
 rm -f /opt/sbs/client/build/static/disclaimer.css
 rm -rf /opt/sbs/server/config/saml/saml
-ln -s /opt/sbs/config/config.yml      /opt/sbs/server/config/config.yml
-ln -s /opt/sbs/config/alembic.ini     /opt/sbs/server/migrations/alembic.ini
-ln -s /opt/sbs/config/saml            /opt/sbs/server/config/saml
-cp /opt/sbs/config/disclaimer.css     /opt/sbs/client/build/static/disclaimer.css
+cp /opt/sbs/config/config.yml      /opt/sbs/server/config/config.yml
+cp /opt/sbs/config/alembic.ini     /opt/sbs/server/migrations/alembic.ini
+cp /opt/sbs/config/disclaimer.css  /opt/sbs/client/build/static/disclaimer.css
+cp /opt/sbs/config/saml            /opt/sbs/server/config/saml
 
 if [ -e "/opt/sbs/cert/frontend.crt" ]
 then
