eb7b073be5705455e41d1753321dec00cb4599e7
Okke Harsta <oharsta@zilverline.com>
2024-12-06 13:58:04 +0100

    Fixed build

--- server/test/api/test_mock_user.py
+++ server/test/api/test_mock_user.py
@@ -1,6 +1,12 @@
+import base64
 import os
 from functools import wraps
 
+from flask import current_app
+from lxml import etree
+from signxml import XMLVerifier
+
+from server.auth.surf_conext import surf_public_signing_certificate
 from server.db.domain import User
 from server.test.abstract_test import AbstractTest
 
@@ -41,4 +47,10 @@ class TestMockUser(AbstractTest):
     def test_eb_interrupt_data(self):
         self.login()
         res = self.get("/api/mock/interrupt_data", query_data={"user_uid": "urn:sarah"}, with_basic_auth=False)
-        self.assertEqual("boa@example.org", "")
+        base64_encoded_xml = res["signed_user"]
+        xml = base64.b64decode(base64_encoded_xml).decode()
+        doc = etree.fromstring(xml)
+        cert = surf_public_signing_certificate(current_app)
+        verified_data = XMLVerifier().verify(doc, x509_cert=cert).signed_xml
+        user_uid = verified_data.attrib.get("user_id")
+        self.assertEqual("urn:sarah", user_uid)
