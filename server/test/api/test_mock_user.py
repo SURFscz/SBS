10d187495d251b35a0c8c9891b470c686c4fa221
Okke Harsta <oharsta@zilverline.com>
2025-03-24 14:31:32 +0100

    Removed lxml references #1692

--- server/test/api/test_mock_user.py
+++ server/test/api/test_mock_user.py
@@ -1,12 +1,6 @@
-import base64
 import os
 from functools import wraps
 
-from flask import current_app
-from lxml import etree
-from signxml import XMLVerifier
-
-from server.auth.surf_conext import surf_public_signing_certificate
 from server.db.domain import User
 from server.test.abstract_test import AbstractTest
 
@@ -46,14 +40,7 @@ class TestMockUser(AbstractTest):
     @allow_for_mock_user_api
     def test_eb_interrupt_data(self):
         self.login()
-        res = self.get("/api/mock/interrupt_data", query_data={"user_uid": "urn:sarah"}, with_basic_auth=False)
-        base64_encoded_xml = res["signed_user"]
-        xml = base64.b64decode(base64_encoded_xml).decode()
-        doc = etree.fromstring(xml)
-        cert = surf_public_signing_certificate(current_app)
-        verified_data = XMLVerifier().verify(doc, x509_cert=cert).signed_xml
-        user_uid = verified_data.attrib.get("user_id")
-        self.assertEqual("urn:sarah", user_uid)
+        self.get("/api/mock/interrupt_data", with_basic_auth=False)
         # In this flow, we don't want to create a mock-user
         self.put("/api/mock", body={"sub": "urn:peter", "name": "Doe Ba", "email": "peter@example.org"},
                  with_basic_auth=False)
