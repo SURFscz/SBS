fa153ba25bb9c2c4ea5f583ccd28593e252fdb7c
Okke Harsta <oharsta@zilverline.com>
2025-05-15 10:40:41 +0200

    Explcit test for JSON set encoding

--- server/test/api/test_dynamic_extended_json_encoder.py
+++ server/test/api/test_dynamic_extended_json_encoder.py
@@ -13,10 +13,11 @@ class TestDynamicExtendedJSONEncoder(AbstractTest):
         _uuid = uuid.uuid4()
         today = dt_today()
 
-        obj = {"1": _uuid, "2": today, "3": "default", "4": (1, 2)}
+        obj = {"1": _uuid, "2": today, "3": "default", "4": (1, 2), "5": {"s", "e", "t"}}
         res = jsonify(obj).json
 
         self.assertEqual(res["1"], str(_uuid))
         self.assertEqual(res["2"], time.mktime(today.timetuple()))
         self.assertEqual(res["3"], "default")
         self.assertListEqual(res["4"], [1, 2])
+        self.assertListEqual(sorted(res["5"]), ["e", "s", "t"])
