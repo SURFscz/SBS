cbca9d37ea4582410195196f1e5ac289aa61ad76
Suzanne van der Meer <suzanne.vandermeer@surf.nl>
2025-05-27 14:55:44 +0200

    attempt to fix a weird camel case issue in cicd

--- client/src/components/autocomplete/Autocomplete.test.jsx
+++ client/src/components/autocomplete/Autocomplete.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import AutoComplete from "./AutoComplete";
+import AutoComplete from "./Autocomplete";
 
 jest.mock("../../locale/I18n", () => ({
   t: key => key
