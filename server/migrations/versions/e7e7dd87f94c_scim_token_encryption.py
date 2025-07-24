36a823a2b1f9315ef599a5a655f8963cf1451595
Eric van Schaik <schaikejgv@gmail.com>
2025-07-24 10:08:14 +0200

    check for config file location, relative to current file path

--- server/migrations/versions/e7e7dd87f94c_scim_token_encryption.py
+++ server/migrations/versions/e7e7dd87f94c_scim_token_encryption.py
@@ -33,7 +33,9 @@ def encrypt_secret(encryption_key: str, plain_secret: str, context: dict) -> str
 
 
 def upgrade():
-    config_file_location = os.environ.get("CONFIG", "config/config.yml")
+    rel_config_file_location = os.environ.get("CONFIG", "config/config.yml")
+    script_dir = os.path.dirname(__file__)
+    config_file_location = os.path.join(script_dir, "..", "..", rel_config_file_location)
     file = f"{config_file_location}"
     with open(file) as f:
         config = munchify(yaml.load(f.read(), Loader=yaml.FullLoader))
