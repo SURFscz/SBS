9ef86f5bd212a1d712e73cf92b4eb8f84575387f
Ricardo van der Heijden <ricardo.vanderheijden@surf.nl>
2026-03-13 10:10:54 +0100

    #2229 Converts CollaborationRequests component to functional component

--- client/src/components/redesign/collaboration-requests/CollaborationRequests.jsx
+++ client/src/components/redesign/collaboration-requests/CollaborationRequests.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback, useEffect, useState} from "react";
 
 import "./CollaborationRequests.scss";
 import {stopEvent} from "../../../utils/Utils";
@@ -10,22 +10,19 @@ import InstituteColumn from "../institute-column/InstituteColumn";
 import UserColumn from "../user-column/UserColumn";
 import {chipTypeForStatus} from "../../../utils/UserRole";
 import {Chip} from "@surfnet/sds";
+import {useQueryParameter} from "../../../hooks/useQueryParameter";
 
 const allValue = "all";
 
-export default class CollaborationRequests extends React.PureComponent {
+const CollaborationRequests = ({organisation, user, history, ...rest}) => {
 
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            filterOptions: [],
-            filterValue: {},
-        }
-    }
+    const [queryFilterValue, setQueryFilterValue] = useQueryParameter('filterValue');
+    const [filterOptions, setFilterOptions] = useState([]);
+    const [filterValue, setFilterValue] = useState({});
 
-    componentDidMount = () => {
-        const {collaboration_requests} = this.props.organisation;
-        const filterOptions = [{
+    const initializeFilters = useCallback(() => {
+        const {collaboration_requests} = organisation;
+        const baseOptions = [{
             label: I18n.t("collaborationRequest.statuses.all", {nbr: collaboration_requests.length}),
             value: allValue
         }];
@@ -42,102 +39,104 @@ export default class CollaborationRequests extends React.PureComponent {
             value: option.status
         })).sort((o1, o2) => o1.label.localeCompare(o2.label));
 
-        this.setState({
-            filterOptions: filterOptions.concat(statusOptions),
-            filterValue: filterOptions[0]
-        });
-    }
+        const allOptions = baseOptions.concat(statusOptions);
+        setFilterOptions(allOptions);
+        setFilterValue(allOptions.find(o => o.value === queryFilterValue) || baseOptions[0]);
+    }, [organisation]);
 
+    useEffect(() => {
+        initializeFilters();
+    }, [initializeFilters]);
 
-    openCollaborationRequest = collaborationRequest => e => {
+    const openCollaborationRequest = collaborationRequest => e => {
         if (e.metaKey || e.ctrlKey) {
             return;
         }
         stopEvent(e);
-        this.props.history.push(`/collaboration-requests/${collaborationRequest.id}`);
+        // Todo: test if this works, this would overwrite the filter in URL
+        history.push(`/collaboration-requests/${collaborationRequest.id}`);
     };
 
-    filter = (filterOptions, filterValue) => {
-        return (
-            <div className="collaboration-request-filter">
-                <Select
-                    className={"collaboration-request-filter-select"}
-                    value={filterValue}
-                    classNamePrefix={"filter-select"}
-                    onChange={option => this.setState({filterValue: option})}
-                    options={filterOptions}
-                    isSearchable={false}
-                    isClearable={false}
-                />
+    const renderFilter = (
+        <div className="collaboration-request-filter">
+            <Select
+                className={"collaboration-request-filter-select"}
+                value={filterValue}
+                classNamePrefix={"filter-select"}
+                onChange={option => {
+                    setQueryFilterValue(option.value);
+                    setFilterValue(option);
+                }}
+                options={filterOptions}
+                isSearchable={false}
+                isClearable={false}
+            />
+        </div>
+    );
+
+    const columns = [
+        {
+            nonSortable: true,
+            key: "logo",
+            header: "",
+            mapper: cr => <Logo src={cr.logo}/>
+        },
+        {
+            key: "name",
+            header: I18n.t("models.collaborations.name"),
+            mapper: cr => <a href={`/collaboration-requests/${cr.id}`}
+                             className={"neutral-appearance"}
+                             onClick={openCollaborationRequest(cr)}>{cr.name}</a>
+        },
+        {
+            key: "units",
+            class: "units",
+            header: I18n.t("units.column"),
+            mapper: cr => <div className="unit-container">
+                {(cr.units || [])
+                    .sort((u1, u2) => u1.name.localeCompare(u2.name))
+                    .map((unit, index) => <span key={index} className="chip-container">
+                    {unit.name}</span>)}
             </div>
-        );
-    }
+        },
+        {
+            key: "status",
+            header: I18n.t("collaborationRequest.status"),
+            mapper: cr => <Chip type={chipTypeForStatus(cr)}
+                                label={I18n.t(`collaborationRequest.statuses.${cr.status}`)}/>
+        },
+        {
+            key: "requester",
+            header: I18n.t("models.collaboration_requests.requester"),
+            mapper: cr => <UserColumn entity={{user: cr.requester}} currentUser={user}/>
+        },
+        {
+            key: "user__schac_home_organisation",
+            header: I18n.t("models.users.institute"),
+            mapper: cr => <InstituteColumn entity={{user: cr.requester}} currentUser={user}/>
+        },
+    ];
 
-    render() {
-        const {organisation, user} = this.props;
-        const {filterOptions, filterValue} = this.state;
-        const columns = [
-            {
-                nonSortable: true,
-                key: "logo",
-                header: "",
-                mapper: cr => <Logo src={cr.logo}/>
-            },
-            {
-                key: "name",
-                header: I18n.t("models.collaborations.name"),
-                mapper: cr => <a href={`/collaboration-requests/${cr.id}`}
-                                 className={"neutral-appearance"}
-                                 onClick={this.openCollaborationRequest(cr)}>{cr.name}</a>
-            },
-            {
-                key: "units",
-                class: "units",
-                header: I18n.t("units.column"),
-                mapper: cr => <div className="unit-container">
-                    {(cr.units || [])
-                        .sort((u1, u2) => u1.name.localeCompare(u2.name))
-                        .map((unit, index) => <span key={index} className="chip-container">
-                        {unit.name}</span>)}
-                </div>
-            },
-            {
-                key: "status",
-                header: I18n.t("collaborationRequest.status"),
-                mapper: cr => <Chip type={chipTypeForStatus(cr)}
-                                    label={I18n.t(`collaborationRequest.statuses.${cr.status}`)}/>
-            },
-            {
-                key: "requester",
-                header: I18n.t("models.collaboration_requests.requester"),
-                mapper: cr => <UserColumn entity={{user: cr.requester}} currentUser={user}/>
-            },
-            {
-                key: "user__schac_home_organisation",
-                header: I18n.t("models.users.institute"),
-                mapper: cr => <InstituteColumn entity={{user: cr.requester}} currentUser={user}/>
-            },
-        ]
-        const filteredCollaborationRequests = filterValue.value === allValue ? organisation.collaboration_requests :
-            organisation.collaboration_requests.filter(cr => cr.status === filterValue.value);
+    const filteredCollaborationRequests = filterValue.value === allValue ? organisation.collaboration_requests :
+        organisation.collaboration_requests.filter(cr => cr.status === filterValue.value);
 
-        const numberOfRequests = organisation.collaboration_requests.length;
-        return (
-            <Entities entities={filteredCollaborationRequests}
-                      modelName={"collaboration_requests"}
-                      searchAttributes={["name", "requester__name"]}
-                      inputFocus={true}
-                      defaultSort="name"
-                      rowLinkMapper={() => this.openCollaborationRequest}
-                      columns={columns}
-                      showNew={false}
-                      customNoEntities={" "}
-                      title={numberOfRequests === 0 ? I18n.t("models.collaboration_requests.noEntities") :
-                          `${I18n.t("models.collaboration_requests.title")} (${numberOfRequests})`}
-                      filters={this.filter(filterOptions, filterValue)}
-                      loading={false}
-                      {...this.props}/>
-        )
-    }
+    const numberOfRequests = organisation.collaboration_requests.length;
+    return (
+        <Entities entities={filteredCollaborationRequests}
+                  modelName={"collaboration_requests"}
+                  searchAttributes={["name", "requester__name"]}
+                  inputFocus={true}
+                  defaultSort="name"
+                  rowLinkMapper={() => openCollaborationRequest}
+                  columns={columns}
+                  showNew={false}
+                  customNoEntities={" "}
+                  title={numberOfRequests === 0 ? I18n.t("models.collaboration_requests.noEntities") :
+                      `${I18n.t("models.collaboration_requests.title")} (${numberOfRequests})`}
+                  filters={renderFilter}
+                  loading={false}
+                  {...rest}/>
+    );
+};
 
-}
+export default CollaborationRequests;
