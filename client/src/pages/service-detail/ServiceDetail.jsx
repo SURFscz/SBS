3118c1cf41c58157d6699ce3cefaa8f1859ab345
Ricardo van der Heijden <ricardo.vanderheijden@surf.nl>
2026-03-17 12:52:16 +0100

    #2229 Prevents applying existing query parameters to changed tab

--- client/src/pages/service-detail/ServiceDetail.jsx
+++ client/src/pages/service-detail/ServiceDetail.jsx
@@ -122,7 +122,7 @@ class ServiceDetail extends React.Component {
             }));
             this.setState({socketSubscribed: true})
         }
-        this.tabChanged(tab, service);
+        this.updateBreadCrumb(service);
         this.setState({
             service: service,
             organisations: organisations,
@@ -314,7 +314,7 @@ class ServiceDetail extends React.Component {
         const serviceId = service ? service.id : this.state.service.id;
         this.updateBreadCrumb(service);
         this.setState({tab: name}, () =>
-            this.props.history.replace(`/services/${serviceId}/${name}${this.props.location.search}`));
+            this.props.history.replace(`/services/${serviceId}/${name}`));
     }
 
     doDeleteMe = () => {
