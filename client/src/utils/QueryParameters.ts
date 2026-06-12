bfb90f2e3d1cccd3eafa328f28dac5efdb8005e8
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2026-06-12 18:00:15 +0200

    #2446 Converts QueryParameters to TS

--- client/src/utils/QueryParameters.ts
+++ client/src/utils/QueryParameters.ts
@@ -1,15 +1,15 @@
-export function replaceQueryParameter(windowLocationSearch, name, value) {
+export function replaceQueryParameter(windowLocationSearch: string, name: string, value: string): string {
     const urlSearchParams = new URLSearchParams(windowLocationSearch);
     urlSearchParams.set(name, value);
     return "?" + urlSearchParams.toString();
 }
 
-export function getParameterByName(name, windowLocationSearch) {
+export function getParameterByName(name: string, windowLocationSearch: string): string | null {
     const urlSearchParams = new URLSearchParams(windowLocationSearch);
     return urlSearchParams.get(name);
 }
 
-export function dictToQueryParams(dict) {
+export function dictToQueryParams(dict: Record<string, string | number | boolean>): string {
     return Object.entries(dict).reduce((acc, item) =>
         acc + `${item[0]}=${encodeURIComponent(item[1])}&`, "?")
 }
