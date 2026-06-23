515be87d431cbe1d02e84140102ef0f30e22447f
Okke Harsta <oharsta@zilverline.com>
2026-06-23 10:16:24 +0200

    Manual cherry-pick for #470

--- server/db/db.py
+++ server/db/db.py
@@ -1,5 +1,7 @@
+import logging
 import os
 
+from alembic.util import CommandError
 from flask_migrate import command
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,4 +15,11 @@ def db_migrations(sqlalchemy_database_uri):
     config = Config(migrations_dir + "alembic.ini")
     config.set_main_option("sqlalchemy.url", sqlalchemy_database_uri)
     config.set_main_option("script_location", migrations_dir)
-    command.upgrade(config, "head")
+    try:
+        command.upgrade(config, "head")
+    except CommandError as e:
+        logger = logging.getLogger("db_migrations")
+        if "Can't locate revision identified by" in e.args[0]:
+            logger.warning(f"Ignoring unknown revision {e}")
+        else:
+            raise e
