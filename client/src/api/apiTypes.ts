eab3e3843d4c17ea81052aa279b923425b00133b
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2026-08-27 15:40:59 +0200

    #2536 Generates types from server app

---
+++ client/src/api/apiTypes.ts
@@ -0,0 +1,85 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+/* This file was automatically generated from pydantic models by running pydantic2ts.
+/* Do not modify it by hand - just update the pydantic models and then re-run the script
+*/
+
+export interface CollaborationDTO {
+  id: number;
+  name: string;
+  description: string;
+  short_name: string;
+  logo: string | null;
+  website_url: string | null;
+  support_email: string | null;
+  organisation_id: number;
+  status: string;
+  expiry_date: string | null;
+  last_activity_date: string;
+  disclose_member_information: boolean | null;
+  disclose_email_information: boolean | null;
+  collaboration_memberships_count: number;
+  organisation: OrganisationDTO;
+  collaboration_memberships: CollaborationMembershipDTO[];
+  groups: GroupDTO[];
+  services: ServiceDTO[];
+}
+export interface OrganisationDTO {
+  id: number;
+  name: string;
+  short_name: string;
+  logo: string | null;
+  accepted_user_policy: string | null;
+  schac_home_organisations: SchacHomeOrganisationDTO[];
+}
+export interface SchacHomeOrganisationDTO {
+  name: string;
+}
+export interface CollaborationMembershipDTO {
+  id: number;
+  user_id: number;
+  role: string;
+  status: string;
+  expiry_date: string | null;
+  created_at: string;
+  user: UserDTO;
+}
+export interface UserDTO {
+  id: number;
+  name: string | null;
+  email: string | null;
+  username: string | null;
+  schac_home_organisation: string | null;
+}
+export interface GroupDTO {
+  id: number;
+  name: string;
+  description: string | null;
+  short_name: string;
+  identifier: string;
+  global_urn: string;
+  auto_provision_members: boolean | null;
+  created_at: string;
+  service_group_id: number | null;
+  collaboration_memberships: CollaborationMembershipDTO[];
+}
+export interface ServiceDTO {
+  id: number;
+  name: string;
+  description: string | null;
+  logo: string | null;
+  uri: string | null;
+  uri_info: string | null;
+  privacy_policy: string | null;
+  accepted_user_policy: string | null;
+  contact_email: string | null;
+  support_email: string | null;
+  token_enabled: boolean | null;
+  token_validity_days: number | null;
+  organisation_name: string | null;
+  service_memberships: ServiceMembershipDTO[];
+}
+export interface ServiceMembershipDTO {
+  user: UserDTO;
+}
