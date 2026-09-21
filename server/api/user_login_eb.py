1eea461cb1ae86df21809a29bdb56051956216ce
Eric van Schaik <eric.vanschaik@surf.nl>
2026-09-21 14:31:04 +0200

    add blanket eduteams epsa to list and add home affiliations to vpea

--- server/api/user_login_eb.py
+++ server/api/user_login_eb.py
@@ -42,9 +42,11 @@ def user_attributes(service: Service, user: User):
             "urn:mace:dir:attribute-def:eduPersonEntitlement": list(all_attributes),  # eduPersonEntitlement
             # If we add them here, we get multi-valued EB uid and eppn because of EB Sram AttributeMerger
             # "urn:mace:dir:attribute-def:uid": [user.uid],  # voPersonID
+            "urn:mace:dir:attribute-def:eduPersonScopedAffiliation": [f"member@{current_app.app_config.eppn_scope.strip()}"],
             "urn:mace:dir:attribute-def:voPersonExternalID": [f"{user.username}@{current_app.app_config.eppn_scope.strip()}"],
             "urn:mace:dir:attribute-def:voPersonID": [user.uid],
             "urn:mace:dir:attribute-def:voPersonSoRID": [user.username],
+            "urn:mace:dir:attribute-def:voPersonExternalAffiliation": [user.scoped_affiliation],
             "urn:mace:surf.nl:attribute-def:ssh-key": [k.ssh_value for k in user.ssh_keys]
         }
     }
